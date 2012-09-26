b9a2baf887988ceb3f6b02cfbaabc5db054bd3ef
sathishkottravel <sathishkottravel@gmail.com>
2012-09-26 18:59:50 +0000

    Added Factory Base

---
+++ include/inviwo/core/inviwofactorybase.h
@@ -0,0 +1,29 @@
+#ifndef IVW_INVIWOFACTORYBASE_H
+#define IVW_INVIWOFACTORYBASE_H
+
+//#include "inviwo/core/inviwo.h"
+#include <string>
+
+namespace inviwo {
+
+    class IvwSerializable;
+
+    class InviwoFactoryBase {
+
+    public:
+        ~InviwoFactoryBase() {}
+
+        template <typename T>
+        static T* instance() {
+            return T::instance();
+        }
+
+        virtual void initialize()=0;
+        virtual void deinitialize()=0;
+
+        virtual IvwSerializable* create(std::string className) const=0;
+    };
+
+} // namespace
+
+#endif // IVW_INVIWOFACETORYBASE_H
