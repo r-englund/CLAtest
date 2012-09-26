7d1c29f0de656e267339cbf8008973b755c7326d
sathishkottravel <sathishkottravel@gmail.com>
2012-09-26 18:33:58 +0000

    adding serialization

---
+++ src/core/io/serialization/inviwofactorybase.cpp
@@ -0,0 +1,14 @@
+#include "inviwo/core/inviwofactorybase.h"
+#include "inviwo/core/io/serialization/ivwserializable.h"
+
+namespace inviwo {
+
+/*
+template <typename T>
+static InviwoFactoryBase* InviwoFactoryBase::instance() {
+    return T::instance();
+}*/
+
+} // namespace
+
+
