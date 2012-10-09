0805d0b9d3c13bc30fb4e6458caf544a7a76c63f
sathishkottravel <sathishkottravel@gmail.com>
2012-10-09 12:45:20 +0000

    code cleanup

--- include/inviwo/core/io/serialization/ivwserializebase.h
+++ include/inviwo/core/io/serialization/ivwserializebase.h
@@ -43,211 +43,75 @@ namespace inviwo {
     typedef ticpp::Iterator<TxAttribute> TxAIt;
 
 
-class InviwoFactoryBase;
-class IvwSerializable;
+    class InviwoFactoryBase;
+    class IvwSerializable;
 
 
-class IvwSerializeBase {
-public:
-    IvwSerializeBase(IvwSerializeBase &s, bool allowReference=false);
-    IvwSerializeBase(std::string fileName, bool allowReference=false);
-    virtual ~IvwSerializeBase();
-    virtual std::string getFileName();
-    //virtual void readFile(std::ostream& stream);
-    //virtual void writeFile(std::ostream& stream);
-    
-    bool isPrimitiveType(const std::type_info& type) const;
-    bool isPrimitivePointerType(const std::type_info& type) const;
-
-    void setAllowReference(const bool &allowReference);
-
-    virtual void registerFactories(void) ;
-
-    template <typename T>
-    T* getRegisteredType(const std::string &className);
-
-    //IvwSerializable* getRegisteredType(const std::string &className);
-
-    template <typename T>
-    T* getNonRegisteredType();
-
-    template <typename T>
-    T* allocateMemory(std::string className);    
-    virtual void setFileName(const std::string fileName);
-
-    /*template <typename T>
-    void serialize(const std::string &key, const std::vector<T> &sVector, const std::string &itemKey);
-
-    void serialize(const std::string &key, const IvwSerializable &sObj);
-
-    void serialize(const std::string &key, const std::string &data);
-
-    void serialize(const std::string &key, const vec2 &data);
-    void serialize(const std::string &key, const vec3 &data);
-    void serialize(const std::string &key, const vec4 &data);
-    void serialize(const std::string &key, const ivec2 &data);
-    void serialize(const std::string &key, const ivec3 &data);
-    void serialize(const std::string &key, const ivec4 &data);
-
-    template<class T>
-    void serialize(const std::string& key, const T* const& data);
-
-    template <typename T>
-    void serializeSTL_Vector(const std::string &key, const T &sVector, const std::string &itemKey);
-
-    void serializePrimitives(const std::string &key, const std::string &data);
-
-    template<typename T>
-    void serializePrimitives(const std::string& key, const T& data);
-
-    template<class T>
-    void serializeVector(const std::string& key, const T& vector, const bool& isColor=false);
-    */
-
-    class NodeSwitch {
+    class IvwSerializeBase {
     public:
-         NodeSwitch(IvwSerializeBase& serializer, TxElement* node);
-        ~NodeSwitch();
+        IvwSerializeBase(IvwSerializeBase &s, bool allowReference=false);
+        IvwSerializeBase(std::string fileName, bool allowReference=false);
+        virtual ~IvwSerializeBase();
+        virtual std::string getFileName();
+        
+        bool isPrimitiveType(const std::type_info& type) const;
+        bool isPrimitivePointerType(const std::type_info& type) const;
 
-    private:  
-        IvwSerializeBase& _serializer;
-        TxElement* _storedNode;
+        void setAllowReference(const bool &allowReference);
+
+        virtual void registerFactories(void) ;
+
+        template <typename T>
+        T* getRegisteredType(const std::string &className);
+
+        template <typename T>
+        T* getNonRegisteredType();
+
+        template <typename T>
+        T* allocateMemory(std::string className);    
+
+        virtual void setFileName(const std::string fileName);
+
+        class NodeSwitch {
+        public:
+             NodeSwitch(IvwSerializeBase& serializer, TxElement* node);
+            ~NodeSwitch();
+
+        private:  
+            IvwSerializeBase& _serializer;
+            TxElement* _storedNode;
+        };
+        
+    protected:
+        friend class NodeSwitch;
+
+        //TODO: These are static factory objects. But still storing them in vectors can be useful??? 
+        std::vector<InviwoFactoryBase *> _registeredFactoryVector;
+        std::string _fileName;
+        TxDocument _doc;
+        TxElement*_root;
+        bool _allowReference;    
     };
-    
-protected:
-    friend class NodeSwitch;
 
-    //TODO: These are static factory objects. But still storing them in vectors can be useful??? 
-    std::vector<InviwoFactoryBase *> _registeredFactoryVector;
-    std::string _fileName;
-    TxDocument _doc;
-    TxElement*_root;
-    bool _allowReference;
-    
-};
-/*
-template <typename T>
-inline T* IvwSerializeBase::getRegisteredType(const std::string &className)
-{
-    T* data = 0;
-    std::vector<InviwoFactoryBase *>::iterator it;
-    for(it = _registeredFactoryVector.begin(); it!=_registeredFactoryVector.end(); it++)
-    {
-        data = dynamic_cast<T*>( (*it)->create(className) ) ;
-        if(data)
-            break;
-    }   
 
-    return data;
-}*/
-
-template <typename T>
-T* IvwSerializeBase::getRegisteredType(const std::string &className)
-{
-    T* data = 0;
-    std::vector<InviwoFactoryBase *>::iterator it;
-    for(it = _registeredFactoryVector.begin(); it!=_registeredFactoryVector.end(); it++)
-    {
-        data = dynamic_cast<T*>( (*it)->create(className) ) ;
-        if(data)
-            break;
-    }
-
-    return data;
-}
-
-template <typename T>
-inline T* IvwSerializeBase::getNonRegisteredType()
-{   
-    return new T();
-}
-
-/*
-template <typename T>
-inline void IvwSerializeBase::serialize(const std::string &key, const std::vector<T> &sVector, const std::string &itemKey) {
-    serializeSTL_Vector(key, sVector, itemKey);
-}
-
-template <typename T>
-inline void IvwSerializeBase::serializeSTL_Vector(const std::string &key, const T &sVector, const std::string &itemKey) {
-    TxElement* newNode = new TxElement(key);
-    _root->LinkEndChild(newNode);
-
-    NodeSwitch tempNodeSwitch(*this, newNode);
-
-    for (typename T::const_iterator it = sVector.begin(); it != sVector.end(); ++it) {
-        if(!isPrimitiveType(typeid(typename T::value_type)) && !isPrimitivePointerType(typeid(typename T::value_type)) ) {
-            serialize(itemKey, (*it)) ;
+    template <typename T>
+    T* IvwSerializeBase::getRegisteredType(const std::string &className) {
+        T* data = 0;
+        std::vector<InviwoFactoryBase *>::iterator it;
+        for(it = _registeredFactoryVector.begin(); it!=_registeredFactoryVector.end(); it++)
+        {
+            data = dynamic_cast<T*>( (*it)->create(className) ) ;
+            if(data)
+                break;
         }
-    }
-}
 
-template<class T>
-inline void IvwSerializeBase::serialize(const std::string& key, const T* const& data) {
-    
-    if(!_allowReference)
-    {
-        //_allowReference = true;
-        serialize(key, *data) ;
-        return;
+        return data;
     }
 
-    TxElement* newNode = new TxElement(key);
-
-    if (data) {
-       
-        //std::string typeString = getTypeString(typeid(*data));
-        //if (!typeString.empty())
-        //newNode->SetAttribute(IvwSerializeConstants::TYPE_ATTRIBUTE, typeid(*data).name());
-            
+    template <typename T>
+    inline T* IvwSerializeBase::getNonRegisteredType() {   
+        return new T();
     }
-
-    _root->LinkEndChild(newNode);
-}
-
-template<class T>
-inline void IvwSerializeBase::serializePrimitives(const std::string& key, const T& data)
-{
-    
-    TxElement* newNode = new TxElement(key);
-    _root->LinkEndChild(newNode);
-    newNode->SetAttribute(IvwSerializeConstants::VALUE_ATTRIBUTE, ss.str());
-}
-
-template<class T>
-inline void IvwSerializeBase::serializeVector(const std::string& key, const T& vector, const bool& isColor)
-{
-    TxElement* newNode = new TxElement(key);
-    _root->LinkEndChild(newNode);
-
-    std::stringstream ss;
-    ss.precision(IvwSerializeConstants::STRINGSTREAM_PRECISION);
-    ss<<(isColor ? static_cast<int>(vector[0]) : vector[0]);
-
-    newNode->SetAttribute(
-        isColor ? IvwSerializeConstants::COLOR_R_ATTRIBUTE : IvwSerializeConstants::VECTOR_X_ATTRIBUTE, ss.str());
-
-    if (vector.length() >= 2) {
-        ss.str(std::string());
-        ss<<(isColor ? static_cast<int>(vector[1]) : vector[1]);
-        newNode->SetAttribute(
-            isColor ? IvwSerializeConstants::COLOR_G_ATTRIBUTE : IvwSerializeConstants::VECTOR_Y_ATTRIBUTE,ss.str());
-    }
-
-    if (vector.length() >= 3) {
-        ss.str(std::string());
-        ss<<(isColor ? static_cast<int>(vector[2]) : vector[2]);
-        newNode->SetAttribute(
-            isColor ? IvwSerializeConstants::COLOR_B_ATTRIBUTE : IvwSerializeConstants::VECTOR_Z_ATTRIBUTE,ss.str());
-    }
-
-    if (vector.length() >= 4) {
-        ss.str(std::string());
-        ss<<(isColor ? static_cast<int>(vector[2]) : vector[2]);
-        newNode->SetAttribute(
-            isColor ? IvwSerializeConstants::COLOR_A_ATTRIBUTE : IvwSerializeConstants::VECTOR_W_ATTRIBUTE,ss.str());
-    }
-}*/
 
 } //namespace
 #endif
--- src/core/io/serialization/ivwserializebase.cpp
+++ src/core/io/serialization/ivwserializebase.cpp
@@ -18,48 +18,20 @@ IvwSerializeBase::NodeSwitch::~NodeSwitch() {
     _serializer._root = _storedNode;
 }
 
-IvwSerializeBase::IvwSerializeBase(IvwSerializeBase &s, bool allowReference): _fileName(s._fileName), _doc(s._fileName), _allowReference(allowReference) {
-    
-    registerFactories();
-    /*try {
-        TxComment* comment;
-
-        TxDeclaration* decl = new TxDeclaration( IvwSerializeConstants::XML_VERSION, "", "" ); 
-	    _doc.LinkEndChild( decl ); 
-     
-        _root = new TxElement(IvwSerializeConstants::INVIWO_TREEDATA);  
-        _root->SetAttribute(IvwSerializeConstants::VERSION_ATTRIBUTE, IvwSerializeConstants::INVIWO_VERSION);
-	    _doc.LinkEndChild( _root );
-
-        comment = new TxComment();
-	    comment->SetValue(IvwSerializeConstants::EDIT_COMMENT.c_str());  
-	    _root->LinkEndChild( comment ); 
-    }
-    catch(TxException& ex) {
-        
-    }*/
+IvwSerializeBase::IvwSerializeBase(IvwSerializeBase &s, bool allowReference)
+    : _fileName(s._fileName)
+    , _doc(s._fileName)
+    , _allowReference(allowReference)
+{
+     registerFactories();  
 }
 
-IvwSerializeBase::IvwSerializeBase(std::string fileName, bool allowReference): _fileName(fileName), _doc(fileName), _allowReference(allowReference) {
-    
-    registerFactories();
-    /*try {
-        TxComment* comment;
-
-        TxDeclaration* decl = new TxDeclaration( IvwSerializeConstants::XML_VERSION, "", "" ); 
-	    _doc.LinkEndChild( decl ); 
-     
-        _root = new TxElement(IvwSerializeConstants::INVIWO_TREEDATA);  
-        _root->SetAttribute(IvwSerializeConstants::VERSION_ATTRIBUTE, IvwSerializeConstants::INVIWO_VERSION);
-	    _doc.LinkEndChild( _root );
-
-        comment = new TxComment();
-	    comment->SetValue(IvwSerializeConstants::EDIT_COMMENT.c_str());  
-	    _root->LinkEndChild( comment ); 
-    }
-    catch(TxException& ex) {
-        
-    }*/
+IvwSerializeBase::IvwSerializeBase(std::string fileName, bool allowReference)
+    : _fileName(fileName)
+    , _doc(fileName)
+    , _allowReference(allowReference)
+{
+    registerFactories(); 
 }
 
 IvwSerializeBase::~IvwSerializeBase() {
@@ -68,7 +40,6 @@ IvwSerializeBase::~IvwSerializeBase() {
 void IvwSerializeBase::registerFactories(void) {
     _registeredFactoryVector.clear();
     _registeredFactoryVector.push_back(InviwoFactoryBase::instance<ProcessorFactory>());
-    //_registeredFactoryVector.push_back(InviwoFactoryBase::instance<PropertyFactory>());
 }
 
 template <typename T>
@@ -120,82 +91,5 @@ bool IvwSerializeBase::isPrimitiveType(const std::type_info& type) const {
 void IvwSerializeBase::setAllowReference(const bool& allowReference) {
     _allowReference = allowReference;
 }
-
-
-/*
-void IvwSerializeBase::serialize(const std::string &key, const IvwSerializable &sObj) {
-    TxElement* newNode = new TxElement(key);
-    _root->LinkEndChild(newNode);
-
-    NodeSwitch tempNodeSwitch(*this, newNode);
-    sObj.serialize(*this) ; 
-}
-
-void IvwSerializeBase::serializePrimitives(const std::string &key, const std::string &data) {
-
-    std::stringstream ss;
-    ss.precision(IvwSerializeConstants::STRINGSTREAM_PRECISION);
-    ss<<data;
-
-     //TxElement* node = new TxElement(key);
-     //_root->LinkEndChild(node);
-     //node->SetAttribute(IvwSerializeConstants::VALUE_ATTRIBUTE, ss.str());
-
-    //TODO: Check key for xml keys
-    _root->SetAttribute(key, ss.str());
-}
-
-void IvwSerializeBase::serialize(const std::string &key, const std::string &data) {
-    serializePrimitives(key, data) ;
-}
-
-void IvwSerializeBase::serialize(const std::string &key, const ivec2 &data) {
-    serializeVector(key, data);
-}
-void IvwSerializeBase::serialize(const std::string &key, const ivec3 &data) {
-    serializeVector(key, data);
-}
-
-void IvwSerializeBase::serialize(const std::string &key, const ivec4 &data) {
-    serializeVector(key, data);
-}
-
-void IvwSerializeBase::serialize(const std::string &key, const vec2 &data) {
-    serializeVector(key, data);
-}
-void IvwSerializeBase::serialize(const std::string &key, const vec3 &data) {
-    serializeVector(key, data);
-}
-
-void IvwSerializeBase::serialize(const std::string &key, const vec4 &data) {
-    serializeVector(key, data);
-}
-
-void IvwSerializeBase::readFile(std::istream& stream) {
-
-    try {
-        _doc.LoadFile();
-        //TxNode* pNode = _doc.FirstChildElement()->NextSibling();
-    }
-    catch(TxException& ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-}
-
-
-void IvwSerializeBase::writeFile(std::ostream& stream) {
-
-    try { 
-        std::cout<<_doc;
-        stream<<_doc;
-        _doc.SaveFile();
-    }
-    catch(TxException& ex) {
-        
-    }
-
-}
-*/
 
 } //namespace
