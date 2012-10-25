dc3ea531f1f5f2ad9ad383a645c380e5c1f47df4
sathishkottravel <sathishkottravel@gmail.com>
2012-10-25 19:06:33 +0000

    render context switch for multiple canvas support ( opengl resources not freed. Note:Fix it in destructors.  )

--- include/inviwo/qt/widgets/canvasqt.h
+++ include/inviwo/qt/widgets/canvasqt.h
@@ -17,17 +17,19 @@ public:
     void initializeGL();
     void resizeGL(int w, int h);
     void paintGL();
-
+    virtual void switchContext();   
+    virtual void update();
     void repaint();
 
     void mousePressEvent(QMouseEvent* e);
     void mouseReleaseEvent (QMouseEvent* e);
     void mouseMoveEvent(QMouseEvent* e);
     void timerEvent(QTimerEvent* e);
+    MouseEvent::MouseButton getMouseButton(QMouseEvent* e);
 
 private:
     static bool glewInitialized_;
-    static QGLWidget* sharedWidget_;
+    static QGLWidget* sharedWidget_; //For rendering-context sharing
     static const std::string logSource_; ///< Source string to be displayed for log messages.
 };
 
