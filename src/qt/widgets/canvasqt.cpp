e7cb3e17d1c2170d59c3c3af5c99996d73568557
sathishkottravel <sathishkottravel@gmail.com>
2012-10-25 19:07:46 +0000

    render context switch for multiple canvas support ( opengl resources not freed. Note:Fix it in destructors.  )

--- src/qt/widgets/canvasqt.cpp
+++ src/qt/widgets/canvasqt.cpp
@@ -14,8 +14,12 @@ CanvasQt::CanvasQt(QWidget* parent)
     : QGLWidget(QGLFormat(QGL::SingleBuffer | QGL::DepthBuffer), parent, sharedWidget_),
     CanvasGL(ivec2(256,256))
 {
-    if (sharedWidget_ == 0)
+    //This is our default rendering context
+    //Intialized once. So "THE" first object of this class will not have any shared context (or widget)
+    //But Following objects, will share the context of initial object
+    if(!sharedWidget_)
         sharedWidget_ = this;
+    //
 }
 
 CanvasQt::~CanvasQt() {}
@@ -26,58 +30,96 @@ void CanvasQt::initialize() {
     show();
 }
 
-void CanvasQt::initializeGL() {
-    LogInfo("Initializing GLEW");
+void CanvasQt::switchContext() {
+    if( context() != context()->currentContext() ) {
+        //LogInfo("Switching Context");
+        QGLWidget::doneCurrent();
+        QGLWidget::makeCurrent();
+    }
+}
+
+void CanvasQt::initializeGL() {    
     if (!glewInitialized_) {
-	    glewInit();
+        LogInfo("Initializing GLEW");
+        glewInit();
         glewInitialized_ = true;
     }
     QGLWidget::initializeGL();
 }
 
 void CanvasQt::resizeGL(int width, int height) {
+    CanvasGL::resize(ivec2(width, height));
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    if (height == 0) gluPerspective(60.0, static_cast<double>(width), 0.1, 10.0);
-    else gluPerspective (60.0, static_cast<double>(width) / static_cast<double>(height), 0.1, 10.0);
+    //if (height == 0) gluPerspective(60.0, static_cast<double>(width), 0.1, 10.0);
+    //else gluPerspective (60.0, static_cast<double>(width) / static_cast<double>(height), 0.1, 10.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
 void CanvasQt::paintGL() {
-    QGLWidget::paintGL();
+    //QGLWidget::paintGL();
+}
+
+void CanvasQt::update() {
+    //QGLWidget::updateGL();
 }
 
 void CanvasQt::repaint() {
-    CanvasGL::repaint();
-    QGLWidget::repaint();
+    //CanvasGL::repaint();
+    //QGLWidget::repaint();
 }
 
 
 void CanvasQt::timerEvent(QTimerEvent* e) {
     IVW_UNUSED_PARAM(e);
-    processorNetworkEvaluator_->evaluate();
-    if (processorNetworkEvaluator_->repaintRequired())
-        repaint();
+    if(processorNetworkEvaluator_) {
+        processorNetworkEvaluator_->evaluate();
+        if (processorNetworkEvaluator_->repaintRequired())
+            update();
+    }
 }
 
+MouseEvent::MouseButton CanvasQt::getMouseButton(QMouseEvent* e) {
+     if(e->buttons() == Qt::LeftButton) {
+         return MouseEvent::MOUSE_BUTTON_LEFT;
+     }
+     else if(e->buttons() == Qt::RightButton) {
+         return MouseEvent::MOUSE_BUTTON_RIGHT;
+     }
+     else if(e->buttons() == Qt::MiddleButton) {
+         return MouseEvent::MOUSE_BUTTON_MIDDLE;
+     }
+     return MouseEvent::MOUSE_BUTTON_NONE;
+}
 
 void CanvasQt::mousePressEvent(QMouseEvent* e) {
-    MouseEvent* mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), MouseEvent::MOUSE_BUTTON_LEFT,
+    if(!processorNetworkEvaluator_) return;
+    MouseEvent* mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), getMouseButton(e),
         MouseEvent::MOUSE_STATE_PRESS, MouseEvent::MODIFIER_NONE, dimensions_);
     processorNetworkEvaluator_->propagateMouseEvent(this, mouseEvent);
 }
 
 void CanvasQt::mouseReleaseEvent (QMouseEvent* e) {
-    MouseEvent* mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), MouseEvent::MOUSE_BUTTON_LEFT,
+    if(!processorNetworkEvaluator_) return;
+    MouseEvent* mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), getMouseButton(e),
         MouseEvent::MOUSE_STATE_RELEASE, MouseEvent::MODIFIER_NONE, dimensions_);
     processorNetworkEvaluator_->propagateMouseEvent(this, mouseEvent);
 }
 
 void CanvasQt::mouseMoveEvent(QMouseEvent*  e) {
-    MouseEvent* mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), MouseEvent::MOUSE_BUTTON_LEFT,
+    if(!processorNetworkEvaluator_) return;
+
+    MouseEvent* mouseEvent = 0;
+    if(e->buttons() == Qt::LeftButton || e->buttons() == Qt::RightButton) {
+        mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), getMouseButton(e),
         MouseEvent::MOUSE_STATE_PRESS, MouseEvent::MODIFIER_NONE, dimensions_);
+    }
+    else {
+        mouseEvent = new MouseEvent(ivec2(e->pos().x(), e->pos().y()), MouseEvent::MOUSE_BUTTON_NONE,
+        MouseEvent::MOUSE_STATE_NONE, MouseEvent::MODIFIER_NONE, dimensions_);
+    }
     processorNetworkEvaluator_->propagateMouseEvent(this, mouseEvent);
 }
 
