c17e7a3b8b12152e86746d54bdc1d8bf298ed068
sathishkottravel <sathishkottravel@gmail.com>
2012-10-30 16:38:32 +0000

    Clear property list widget and supporting functions for multiple processor (from rectangle selection)

--- src/qt/editor/propertylistwidget.cpp
+++ src/qt/editor/propertylistwidget.cpp
@@ -12,37 +12,96 @@ PropertyListWidget::PropertyListWidget(QWidget* parent) : InviwoDockWidget(tr("P
     setMinimumWidth(200);
     propertyListWidget_ = this;
     propertyWidgetFactory_ = new PropertyWidgetFactoryQt();
+
+    listWidget_ = new QWidget(propertyListWidget_);
+    listWidgetLayout_ = new QVBoxLayout(listWidget_);
+    listWidgetLayout_->setAlignment(Qt::AlignTop);
+    setWidget(listWidget_);
 }
 
 PropertyListWidget::~PropertyListWidget() {}
 
+void PropertyListWidget::showProcessorProperties(std::vector<Processor*> processors) {
+    for (size_t i=0; i<processors.size(); i++) {
+        addProcessorPropertiesToLayout(processors[i]);
+    }
+}
+
 void PropertyListWidget::showProcessorProperties(Processor* processor) {
     // check if processor widget has been already generated
     std::map<std::string, QWidget*>::iterator it = propertyWidgetMap_.find(processor->getIdentifier());
+    QWidget* processorPropertyWidget = 0;
     if (it != propertyWidgetMap_.end()) {
         // property widget has already been created and stored in the map
-        setWidget(it->second);
+        processorPropertyWidget = it->second;
     } else {
-        // create property widget and store it in the map
-        QWidget* processorPropertyWidget = new QWidget();
-        QVBoxLayout* vLayout = new QVBoxLayout(processorPropertyWidget);
-        vLayout->setAlignment(Qt::AlignTop);
+        processorPropertyWidget = createNewProcessorPropertiesItem(processor);
+    }
 
-        QLabel* processorLabel = new QLabel(QString::fromStdString(processor->getIdentifier()));
-        processorLabel->setAlignment(Qt::AlignCenter);
-        processorLabel->setAutoFillBackground(true);
-        processorLabel->setFrameStyle(QFrame::StyledPanel);
-        
-        vLayout->addWidget(processorLabel);
-        std::vector<Property*> properties = processor->getProperties();
-        for (size_t i=0; i<properties.size(); i++) {
-            Property* curProperty = properties[i];
-            PropertyWidgetQt* propertyWidget = propertyWidgetFactory_->create(curProperty);
-            vLayout->addWidget(propertyWidget);
-            curProperty->registerPropertyWidget(propertyWidget);
-        }
-        setWidget(processorPropertyWidget);
-        propertyWidgetMap_.insert(std::make_pair(processor->getIdentifier(), processorPropertyWidget));
+    if(processorPropertyWidget) {
+        removeAllProcessorProperties();
+        processorPropertyWidget->setVisible(true);
+        listWidgetLayout_->addWidget(processorPropertyWidget);
+    }
+}
+
+void PropertyListWidget::addProcessorPropertiesToLayout(Processor* processor) {
+    // check if processor widget has been already generated
+    std::map<std::string, QWidget*>::iterator it = propertyWidgetMap_.find(processor->getIdentifier());
+    QWidget* processorPropertyWidget = 0;
+    if (it != propertyWidgetMap_.end()) {
+        // property widget has already been created and stored in the map
+        processorPropertyWidget = it->second;
+    } else {
+        processorPropertyWidget = createNewProcessorPropertiesItem(processor); 
+    }
+
+    if(processorPropertyWidget) {
+        processorPropertyWidget->setVisible(true);
+        listWidgetLayout_->addWidget(processorPropertyWidget);
+    }
+}
+
+void PropertyListWidget::removeAllProcessorProperties() {
+    while (QLayoutItem* item = listWidgetLayout_->takeAt(0)) {
+        QWidget* processorPropertyWidget = item->widget();
+        listWidgetLayout_->removeWidget(processorPropertyWidget);
+        processorPropertyWidget->setVisible(false);
+    }
+}
+
+QWidget* PropertyListWidget::createNewProcessorPropertiesItem(Processor* processor) {
+    // create property widget and store it in the map
+    QWidget* processorPropertyWidget = new QWidget();
+    QVBoxLayout* vLayout = new QVBoxLayout(processorPropertyWidget);
+    vLayout->setAlignment(Qt::AlignTop);
+
+    QLabel* processorLabel = new QLabel(QString::fromStdString(processor->getIdentifier()));
+    processorLabel->setAlignment(Qt::AlignCenter);
+    processorLabel->setAutoFillBackground(true);
+    processorLabel->setFrameStyle(QFrame::StyledPanel);
+
+    vLayout->addWidget(processorLabel);
+    std::vector<Property*> properties = processor->getProperties();
+    for (size_t i=0; i<properties.size(); i++) {
+        Property* curProperty = properties[i];
+        PropertyWidgetQt* propertyWidget = propertyWidgetFactory_->create(curProperty);
+        vLayout->addWidget(propertyWidget);
+        curProperty->registerPropertyWidget(propertyWidget);
+    } 
+    propertyWidgetMap_.insert(std::make_pair(processor->getIdentifier(), processorPropertyWidget));
+
+    return processorPropertyWidget;
+}
+
+void PropertyListWidget::removeProcessorProperties(Processor* processor) {
+    std::map<std::string, QWidget*>::iterator it = propertyWidgetMap_.find(processor->getIdentifier());
+    if (it != propertyWidgetMap_.end()) { 
+        QWidget* processorPropertyWidget = propertyWidgetMap_[processor->getIdentifier()];
+        listWidgetLayout_->removeWidget(processorPropertyWidget);
+        processorPropertyWidget->setVisible(false);
+        propertyWidgetMap_.erase(processor->getIdentifier());
+        delete processorPropertyWidget;
     }
 }
 
