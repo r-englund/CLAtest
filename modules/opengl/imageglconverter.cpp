05232ff2044911d3b8f3bd280d22cfd3b0991e39
sathishkottravel <sathishkottravel@gmail.com>
2012-11-06 13:44:37 +0000

    Minor Fix

--- modules/opengl/imageglconverter.cpp
+++ modules/opengl/imageglconverter.cpp
@@ -1,4 +1,5 @@
 #include "imageglconverter.h"
+#include "inviwo/core/datastructures/imagerepresentation.h"
 
 namespace inviwo {
 
@@ -8,8 +9,13 @@ namespace inviwo {
 
     ImageGLConverter::~ImageGLConverter() {}
 
-    void ImageGLConverter::convert(DataRepresentation* /*source*/, DataRepresentation* destination) {
-        destination = new ImageGL();
+    void ImageGLConverter::convert(DataRepresentation* source, DataRepresentation* destination) {
+        ImageRepresentation* imageRepresentation = dynamic_cast<ImageRepresentation*>(source);
+        if (imageRepresentation) {
+            destination = new ImageGL(imageRepresentation->size());
+            return;
+        }
+        destination = 0;
     }
 
 } // namespace
