3ed403546ec8fc87741f593b133de00656c148d0
alejo849 <alejo849@student.liu.se>
2013-01-17 08:27:23 +0000

    Implementation of the intVec2 QtWidget

---
+++ src/qt/widgets/properties/intvec2propertywidgetqt.cpp
@@ -0,0 +1,54 @@
+#include "inviwo/qt/widgets/properties/intvec2propertywidgetqt.h"
+#include <QHBoxLayout>
+#include <QLabel>
+
+namespace inviwo {
+
+IntVec2PropertyWidgetQt::IntVec2PropertyWidgetQt(IntVec2Property *property) {
+	generateWidget();
+	updateFromProperty();
+	}
+
+void IntVec2PropertyWidgetQt::generateWidget() {
+	QHBoxLayout* hLayout = new QHBoxLayout();
+	hLayout->addWidget(new QLabel(QString::fromStdString(property_->getDisplayName())));
+
+	QWidget* sliderWidget = new QWidget();
+	QVBoxLayout* vLayout = new QVBoxLayout();
+	sliderWidget->setLayout(vLayout);
+	sliderX_ = new QSlider(Qt::Horizontal);
+	//sliderX_->setOrientation(Qt::Horizontal);
+	vLayout->addWidget(sliderX_);
+	sliderY_ =new QSlider(Qt::Horizontal);
+	vLayout->addWidget(sliderY_);
+	hLayout->addWidget(sliderWidget);
+	connect(sliderX_, SIGNAL(valueChanged(int)), this, SLOT(setXValue(int)));
+	connect(sliderY_, SIGNAL(valueChanged(int)), this, SLOT(setYValue(int)));
+	setLayout(hLayout);
+}
+
+void IntVec2PropertyWidgetQt::setXValue(int value) {
+	vec2 valueVec2 = property_->get();
+	valueVec2.x = value;
+	property_->set(valueVec2);
+}
+
+void IntVec2PropertyWidgetQt::setYValue(int value) {
+	vec2 valueVec2 = property_->get();
+	valueVec2.y = value;
+	property_->set(valueVec2);
+}
+
+void IntVec2PropertyWidgetQt::setPropertyValue() {
+	setXValue(sliderX_->value());
+	setYValue(sliderX_->value());
+}
+
+void IntVec2PropertyWidgetQt::updateFromProperty() {
+	vec2 valueVec2 = property_->get();
+	sliderX_->setValue(sliderX_->minimum()+valueVec2.x);
+	sliderY_->setValue(sliderY_->minimum()+valueVec2.y);
+};
+
+
+} //namespace
