66523a574a60cf0d3b4e0bbf4f4e289efd2e6438
hanhe945 <hanhe945@student.liu.se>
2013-01-27 23:18:29 +0000

    Fixed some (not all) bugs with the rotating and implemented a basic zoom. The zoom needs more work.

--- src/core/interaction/trackball.cpp
+++ src/core/interaction/trackball.cpp
@@ -1,8 +1,11 @@
 #include "inviwo/core/interaction/trackball.h"
+//#include <iostream>
 
 namespace inviwo {
 
-    static const float MOVEMENT_EPSILON = 0.0001f;
+    static const float MOVEMENT_EPSILON = 0.0001f;	
+	static const float ZOOM_FACTOR = 0.05f;
+	bool mouseHold = false;
 
     Trackball::Trackball(CameraProperty* camera)
         : InteractionHandler(),
@@ -14,9 +17,9 @@ namespace inviwo {
 
     vec3 Trackball::mapNormalizedMousePosToTrackball(vec2 mousePos) {
         // set x and y to lie in interval [-0.5, 0.5]
-        vec3 result = vec3(mousePos.x-0.5f, mousePos.y*-0.5f, 0.0f);
+        vec3 result = vec3(mousePos.x-0.5f, mousePos.y-0.5f, 0.0f);
         // compute z = sqrt(r-x^2-y^2) with r=0.5
-        result.z = 0.5f - mousePos.x*mousePos.x - mousePos.y*mousePos.y;
+        result.z = 0.5f - result.x*result.x - result.y*result.y;
         result.z = result.z > 0.0f ? sqrtf(result.z) : 0.0f;
         return glm::normalize(result);
     }
@@ -26,15 +29,23 @@ namespace inviwo {
         if (mouseEvent) {
         if (mouseEvent->button() == MouseEvent::MOUSE_BUTTON_LEFT && mouseEvent->state() == MouseEvent::MOUSE_STATE_PRESS) {
             ivec2 curMousePos = mouseEvent->pos();
-            vec3 curTrackballPos = mapNormalizedMousePosToTrackball(mouseEvent->posNormalized());                       
+            vec3 curTrackballPos = mapNormalizedMousePosToTrackball(mouseEvent->posNormalized());
+			
+			// disable movements on first press
+			if (!mouseHold){				
+				lastTrackballPos_ = curTrackballPos;
+				mouseHold = true;
+			}
+
             if (curTrackballPos != lastTrackballPos_) {
                 // calculate rotation angle
-                float rotationAngle = acos(glm::dot(curTrackballPos, lastTrackballPos_ ));
-
+				float rotationAngle = acos(glm::dot(curTrackballPos, lastTrackballPos_ ));
+				
                 // obtain rotation axis
-                vec3 rotationAxis = glm::cross(lastTrackballPos_, curTrackballPos);
-
-                // generate quaternion and rotate camera                
+                vec3 rotationAxis = glm::cross(curTrackballPos, lastTrackballPos_);
+				//std::cout << rotationAxis.x << " " << rotationAxis.y << " " << rotationAxis.z << std::endl;
+                
+				// generate quaternion and rotate camera                
                 rotationAxis = glm::normalize(rotationAxis);
                 quat quaternion = glm::angleAxis(rotationAngle*180.0f/3.14f, rotationAxis);
                 camera_->setLookFrom(glm::rotate(quaternion, camera_->lookFrom()));
@@ -46,19 +57,28 @@ namespace inviwo {
                 lastTrackballPos_ = curTrackballPos;
             }
         } else if (mouseEvent->button() == MouseEvent::MOUSE_BUTTON_RIGHT && mouseEvent->state() == MouseEvent::MOUSE_STATE_PRESS) {
-            //std::cout << "zooming" << std::endl;
-            float zoomFactor = 1.0f;
+            vec3 diff = vec3(0);
             ivec2 curMousePos = mouseEvent->pos();
-            vec3 curTrackballPos = mapNormalizedMousePosToTrackball(mouseEvent->posNormalized());                       
-            if (curTrackballPos != lastTrackballPos_) {
-                vec3 zoomVec = (lastTrackballPos_-curTrackballPos)*zoomFactor;
-                camera_->setLookTo(camera_->lookTo()+zoomVec);
-                camera_->invalidate();
 
-                lastMousePos_ = curMousePos;
-                lastTrackballPos_ = curTrackballPos;
-            }
-        }
+			// disable movements on first press
+			if (!mouseHold){				
+				lastMousePos_ = curMousePos;
+				mouseHold = true;
+			}
+
+			if (curMousePos != lastMousePos_){
+				// use the difference in mouse y-position to determine amount of zoom
+				diff.z = curMousePos.y - lastMousePos_.y;
+				// TODO: Zoom in a better fashion 
+				camera_->setLookTo(camera_->lookTo()+diff*ZOOM_FACTOR);     
+				camera_->invalidate();
+				lastMousePos_ = curMousePos;
+			}
+			
+		} else if (mouseEvent->state() == MouseEvent::MOUSE_STATE_RELEASE) {
+			mouseHold = false;
+			//std::cout << "Release" << std::endl;
+		}
             return;
         }
 
