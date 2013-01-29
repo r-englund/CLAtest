e0cc194cd7ad13434fe33245945eb4322d1965b2
sathishkottravel <sathishkottravel@gmail.com>
2013-01-29 23:55:08 +0000

    adding source files

---
+++ include/inviwo/core/util/callback.h
@@ -0,0 +1,60 @@
+#ifndef IVW_CALLBACK_H
+#define IVW_CALLBACK_H
+
+#include <inviwo/core/inviwocoredefine.h>
+#include <inviwo/core/inviwo.h>
+
+namespace inviwo {
+
+class IVW_CORE_API BaseCallBack {
+public:
+    virtual void invoke()=0;
+};
+
+
+template <typename T>
+class IVW_CORE_API MemberFunctionCallBack : public BaseCallBack {
+public:
+    typedef void (T::*fPointerType1)();
+
+    MemberFunctionCallBack(T* obj, fPointerType1 functionPtr) 
+        : functionPtr1_(functionPtr)
+        , obj_(obj){}
+
+    virtual void invoke() {
+        if (functionPtr1_) (*obj_.*functionPtr1_)();
+    }
+
+private:
+    fPointerType1 functionPtr1_;
+    T* obj_;
+};
+
+
+// Example usage
+// CallBackList cbList;
+// cbList.addMemberFunction(&myClassObject, &MYClassObject::myFunction);
+
+//TODO: Use map that uses string as keys : CallBackMap
+class IVW_CORE_API CallBackList {
+public:
+    typedef std::vector<BaseCallBack*>::iterator cblIt;
+    CallBackList() {}
+    void invokeAll() { 
+        for (cblIt it=callBackList_.begin(); it!=callBackList_.end(); ++it)
+            (*it)->invoke();        
+    }
+
+    template <typename T>
+    void addMemberFunction(T* o, void (T::*m)()) {
+        callBackList_.push_back(new MemberFunctionCallBack<T>(o, m)); 
+    }
+
+private:
+    std::vector<BaseCallBack*> callBackList_;
+};
+
+
+} // namespace
+
+#endif // IVW_CALLBACK_H
---
+++ src/core/util/callback.cpp
@@ -0,0 +1,6 @@
+#include <inviwo/core/util/callback.h>
+#include <inviwo/core/inviwo.h>
+
+namespace inviwo {
+   
+}; // namespace
