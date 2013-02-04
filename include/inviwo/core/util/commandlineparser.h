80c6b1f15a428e9e0a67e827449b3c3b85c44351
emawi895 <emawi895@student.liu.se>
2013-02-04 08:15:37 +0000

    Started implementation of CommandLineParser as part of #47. Not yet finished, though.

---
+++ include/inviwo/core/util/commandlineparser.h
@@ -0,0 +1,56 @@
+#ifndef IVW_COMMANDLINEPARSER_H
+#define IVW_COMMANDLINEPARSER_H
+
+#include <inviwo/core/inviwocoredefine.h>
+#include <inviwo/core/inviwo.h>
+
+#include <tclap/CmdLine.h>
+
+namespace inviwo {
+
+/** \brief Wrapper class to handle command line parsing.
+ *  
+ *  Wraps around TCLAP to provide command line argument parsing.
+ *
+ *  TODO: Change into an abstract, general, class and then a derived subclass,
+ *  which contains the TCLAP specific parts.
+ *  
+ *  TODO2: Not yet fully implemented
+ */
+class IVW_CORE_API CommandLineParser {
+
+public:
+    CommandLineParser();
+    virtual ~CommandLineParser();
+
+    virtual void initialize();
+    virtual void deinitialize();
+
+    void parse(int argc, char** argv) {
+        cmd_.parse(argc, argv);
+    }
+
+    void setArgc(int argc) {
+        argc_ = argc;
+    }
+
+    void setArgv(char** argv) {
+        argv_ = argv;
+    }
+    //virtual std::string getWorkspace();
+    //virtual bool getExitWithCapture();
+    // TODO: No need to serialize the parser, right?
+    //virtual void serialize(IvwSerializer& s) const;
+    //virtual void deserialize(IvwDeserializer& d);
+
+protected:
+    TCLAP::CmdLine cmd_;
+
+private:
+    int argc_;
+    char** argv_;
+};
+
+} // namespace
+
+#endif // IVW_COMMANDLINEPARSER_H
---
+++ src/core/util/commandlineparser.cpp
@@ -0,0 +1,19 @@
+#include <inviwo/core/util/commandlineparser.h>
+
+namespace inviwo {
+
+    CommandLineParser::CommandLineParser()
+        : cmd_("Inviwo description...", ' ', "0.0.0") {}
+
+CommandLineParser::~CommandLineParser() {}
+
+void CommandLineParser::initialize() {
+    // Set up available arguments and flags
+    //TCLAP::ValueArg<std::string> workspaceArg("o", "workspace", "Workspace to open", false, "", "Path to workspace");
+    //cmd_.add( workspaceArg );
+}
+
+void CommandLineParser::deinitialize() {
+}
+
+} // namespace
