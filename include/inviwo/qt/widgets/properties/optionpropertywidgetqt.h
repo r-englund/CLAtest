3a586d22d1e6782bd779348fbee1285b02f4353e
alejo849 <alejo849@student.liu.se>
2013-02-11 11:50:09 +0000

    Compile fix

--- include/inviwo/qt/widgets/properties/optionpropertywidgetqt.h
+++ include/inviwo/qt/widgets/properties/optionpropertywidgetqt.h
@@ -1,36 +1,50 @@
-#ifndef IVW_OPTIONPROPERTYWIDGETQT_H
-#define IVW_OPTIONPROPERTYWIDGETQT_H
-
-#include <inviwo/qt/widgets/inviwoqtwidgetsdefine.h>
-
-#include <QComboBox>
-
-#include <inviwo/qt/widgets/properties/propertywidgetqt.h>
-
-#include <inviwo/core/properties/optionproperty.h>
-
-namespace inviwo {
-
-class IVW_QTWIDGETS_API OptionPropertyWidgetQt : public PropertyWidgetQt {
-
-    Q_OBJECT;
-
-public:
-    OptionPropertyWidgetQt(OptionProperty* property);
-
-    void updateFromProperty();
-
-private:
-    OptionProperty* property_;
-    QComboBox* comboBox_;
-
-
-    void generateWidget();
-
-public slots:
-    void setPropertyValue();
-};
-
-} // namespace
-
-#endif // IVW_OPTIONPROPERTYWIDGETQT_H
+//#ifndef IVW_OPTIONPROPERTYWIDGETQT_H
+//#define IVW_OPTIONPROPERTYWIDGETQT_H
+//
+//#include <inviwo/qt/widgets/inviwoqtwidgetsdefine.h>
+//
+//#include <QComboBox>
+//
+//#include <inviwo/qt/widgets/properties/propertywidgetqt.h>
+//
+//#include <inviwo/core/properties/stringoptionproperty.h>
+//
+//namespace inviwo {
+//
+////    class Base :public PropertyWidgetQt
+////{
+////    Q_OBJECT
+////public:
+////    Base( QWidget* parent = 0 );
+////    ~Base();
+////
+////public slots:
+////    void setPropertyValue();
+////};
+//
+//
+//class IVW_QTWIDGETS_API OptionPropertyWidgetQt : public PropertyWidgetQt {
+//
+//    Q_OBJECT;
+//
+//public:
+//
+//    OptionPropertyWidgetQt(StringOptionProperty* property);
+//
+//    void updateFromProperty();
+//
+//private:
+//
+//    StringOptionProperty* property_;
+//    QComboBox* comboBox_;
+//
+//    void generateWidget();
+//    void fillComboBox();
+//
+////public slots:
+////    void setPropertyValue();
+//};
+//
+//} // namespace
+//
+//#endif // IVW_OPTIONPROPERTYWIDGETQT_H
