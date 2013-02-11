4a7c053aee00ac9e0f37d2279f0f4aab43e2c983
sathishkottravel <sathishkottravel@gmail.com>
2013-02-11 10:08:21 +0000

    compile fix

--- src/qt/widgets/properties/propertywidgetfactoryqt.cpp
+++ src/qt/widgets/properties/propertywidgetfactoryqt.cpp
@@ -9,8 +9,8 @@
 #include <inviwo/core/properties/scalarproperties.h>
 #include <inviwo/core/properties/snapshotproperty.h>
 #include <inviwo/core/properties/stringproperty.h>
-#include <inviwo/core/properties/stringoptionproperty.h>
-#include <inviwo/core/properties/templatedoptionproperty.h>
+//#include <inviwo/core/properties/stringoptionproperty.h>
+//#include <inviwo/core/properties/templatedoptionproperty.h>
 #include <inviwo/core/properties/texteditorproperty.h>
 #include <inviwo/core/properties/transferproperty.h>
 #include <inviwo/core/properties/vectorproperties.h>
