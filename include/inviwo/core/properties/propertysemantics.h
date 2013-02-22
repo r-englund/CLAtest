0f1ca5bb7518edcc44bec2a9c5029b3f8a949ef7
alejo849 <alejo849@student.liu.se>
2013-02-22 13:25:22 +0000

    Forgot one file....

---
+++ include/inviwo/core/properties/propertysemantics.h
@@ -0,0 +1,14 @@
+#ifndef IVW_PROPERTYSEMANTCIS_H
+#define IVW_PROPERTYSEMANTCIS_H
+
+namespace inviwo {
+    class IVW_CORE_API PropertySemantics {
+
+    public:
+        enum Type { Color =1,
+                    LightPosition =2,
+                    Editor=3};
+    };
+}
+
+#endif //IVW_PROPERTYSEMANTCIS_H
