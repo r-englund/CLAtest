f9c3b395c345c443450e574fdd153ec8101ff3c2
eriksunden <eriksunden85@gmail.com>
2013-02-25 21:00:27 +0000

    Checked in missing declarations

--- src/core/datastructures/volumeram.cpp
+++ src/core/datastructures/volumeram.cpp
@@ -1,13 +1,18 @@
 #include <inviwo/core/datastructures/volumeram.h>
+#include <inviwo/core/util/typetostring.h>
 
 namespace inviwo {
 
     VolumeRAM::VolumeRAM()
-        : VolumeRepresentation(uvec3(128,128,128), "UINT8"), data_(0)
+        : VolumeRepresentation(uvec3(128,128,128), "UINT8"), data_(0), bytesPerVoxel_(0)
     {}
 
     VolumeRAM::VolumeRAM(uvec3 dimensions)
-        : VolumeRepresentation(dimensions, "UINT8"), data_(0)
+        : VolumeRepresentation(dimensions, "UINT8"), data_(0), bytesPerVoxel_(0)
+    {}
+
+    VolumeRAM::VolumeRAM(uvec3 dimensions, const VolumeBorders& border)
+        : VolumeRepresentation(dimensions, "UINT8", border), data_(0), bytesPerVoxel_(0)
     {}
 
     VolumeRAM::~VolumeRAM() {}
@@ -18,6 +23,18 @@ namespace inviwo {
 
     void* VolumeRAM::getData() {
         return data_;
+    }
+
+    const void* VolumeRAM::getData() const{
+        return const_cast<void*>(data_);
+    }
+
+    size_t VolumeRAM::getBitsPerVoxel() const{
+        return getBytesPerVoxel()*8;
+    }
+
+    size_t VolumeRAM::getBytesPerVoxel() const{
+        return bytesPerVoxel_;
     }
 
 } // namespace
