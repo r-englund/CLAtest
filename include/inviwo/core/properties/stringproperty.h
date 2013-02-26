9a175ae861cc4c890188a0601028fd87677fd54e
alejo849 <alejo849@student.liu.se>
2013-02-26 08:31:21 +0000

    Fix

--- include/inviwo/core/properties/stringproperty.h
+++ include/inviwo/core/properties/stringproperty.h
@@ -8,18 +8,17 @@ namespace inviwo {
     class IVW_CORE_API StringProperty : public TemplateProperty<std::string> {
 
     public:
-        
-        /** 
+        /**
          * \brief Constructor for the StringProperty
          *
          * The PropertySemantics can be set to Editor. Then a TextEditorWidget will be used instead of a StringPropertyWidget
-         * 
-         * 
-         * @param std::string identifier identifier for the property 
+         *
+         *
+         * @param std::string identifier identifier for the property
          * @param std::string displayName displayName for the property
          * @param std::string value value of the property
          * @param PropertySemantics::Type semantics Semantics of the property, if nothing is specified it will be the Default. The semantics can be specified as Editor
-         * @return  
+         * @return 
          */
         StringProperty(std::string identifier, std::string displayName, std::string value, PropertySemantics::Type semantics = PropertySemantics::Default);
         virtual void serialize(IvwSerializer& s) const;
