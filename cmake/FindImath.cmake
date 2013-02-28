a20ada0272e58e8407de9550b50c27eb1ba24669
eriksunden <eriksunden85@gmail.com>
2013-02-28 09:32:12 +0000

    Removed unused cmake-files which destroyed dependency order.

--- cmake/FindImath.cmake
+++
@@ -1,7 +0,0 @@
-# - Find Imath
-# Find Imath headers and libraries.
-#
-#  IMATH_INCLUDE_DIRS - where to find IMATH uncludes.
-#  IMATH_LIBRARIES    - List of libraries when using IMATH.
-#  IMATH_FOUND        - True if IMATH found.
-
--- cmake/FindZLIB.cmake
+++
@@ -1,7 +0,0 @@
-# - Find zlib
-# Find the native ZLIB includes and library
-#
-#  ZLIB_INCLUDE_DIRS   - where to find zlib.h, etc.
-#  ZLIB_LIBRARIES      - List of libraries when using zlib.
-#  ZLIB_FOUND          - True if zlib found.
-#
