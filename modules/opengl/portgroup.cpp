cdc0892db66151703e11c1bad9e6744c230fa94c
timoropinski <timo.ropinski@gmail.com>
2013-06-12 06:44:32 +0000

    added missing files

---
+++ modules/opengl/portgroup.cpp
@@ -0,0 +1,67 @@
+#include "portgroup.h"
+
+#include <modules/opengl/image/imagegl.h>
+
+#include <sstream>
+
+namespace inviwo {
+
+PortGroup::PortGroup()
+    : fbo_()
+{}
+
+void PortGroup::initialize() {
+    fbo_ = new FrameBufferObject();
+}
+
+void PortGroup::deinitialize() {
+    delete fbo_;
+    fbo_ = 0;
+}
+
+void PortGroup::activate() {
+    ivwAssert(fbo_!=0, "PortGroup not initialized.");
+    GLenum* attachementIDs = new GLenum[ports_.size()];
+    std::vector<GLenum> attachementIDVector = fbo_->getAttachementIDs();
+    for (size_t i=0; i<ports_.size(); i++)
+        attachementIDs[i] = attachementIDVector[i];
+    glDrawBuffers(ports_.size(), attachementIDs);
+    LGL_ERROR;
+    delete attachementIDs;
+}
+
+void PortGroup::deactivate() {
+    ivwAssert(fbo_!=0, "PortGroup not initialized.");
+    fbo_->deactivate();
+}
+
+void PortGroup::addPort(ImageOutport port) {
+    ports_.push_back(&port);
+    reattachTargets();
+}
+
+void PortGroup::reattachTargets() {
+    ivwAssert(fbo_!=0, "PortGroup not initialized.");
+    ivwAssert(!ports_.empty(), "PortGroup is empty.");
+    fbo_->activate();
+    fbo_->detachAllTextures();
+    for (size_t i=0; i<ports_.size(); i++) {
+        const Image* image = ports_[i]->getConstData();
+        if (image) {
+            ImageGL* imageGL = const_cast<ImageGL*>(image->getRepresentation<ImageGL>());
+            fbo_->attachTexture(imageGL->getColorTexture(), static_cast<GLenum>(GL_COLOR_ATTACHMENT0_EXT+i));
+            if (i==0)
+                // depth values only valid for the first render target
+                fbo_->attachTexture(imageGL->getDepthTexture(), static_cast<GLenum>(GL_DEPTH_ATTACHMENT_EXT));
+        }
+    }
+}
+
+void PortGroup::addShaderDefines(Shader* shader) {
+    for (size_t i=0; i<ports_.size(); i++) {
+        shader->getFragmentShaderObject()->addShaderDefine("OP"+i);
+        shader->getFragmentShaderObject()->addOutDeclaration("FragData"+i);
+    }
+}
+
+} // namespace
---
+++ modules/opengl/portgroup.h
@@ -0,0 +1,37 @@
+#ifndef IVW_PORTGROUP_H
+#define IVW_PORTGROUP_H
+
+#include <modules/opengl/openglmoduledefine.h>
+#include <inviwo/core/common/inviwo.h>
+
+#include <modules/opengl/glwrap/framebufferobject.h>
+#include <modules/opengl/glwrap/shader.h>
+
+namespace inviwo {
+
+class IVW_MODULE_OPENGL_API PortGroup {
+
+public:
+    PortGroup();
+
+    void initialize();
+    void deinitialize();
+
+    void activate();
+    void deactivate();
+
+    void addPort(ImageOutport port);
+
+    void reattachTargets();
+
+    void addShaderDefines(Shader* shader);
+
+private:
+    FrameBufferObject* fbo_;
+
+    std::vector<ImageOutport*> ports_;
+};
+
+} // namespace
+
+#endif // IVW_PORTGROUP_H
