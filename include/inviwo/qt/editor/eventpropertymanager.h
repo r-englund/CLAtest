aa7307a1942948c48bd1c46e6f3de1f71818ba88
hanhe945 <hanhe945@student.liu.se>
2013-06-20 08:16:56 +0000

    Doxy commenting

--- include/inviwo/qt/editor/eventpropertymanager.h
+++ include/inviwo/qt/editor/eventpropertymanager.h
@@ -9,23 +9,85 @@
 
 namespace inviwo {
 
+/** class EventPropertyManager
+* 
+* A manager which finds and contain all eventproperties in the workspace.
+* All remapping of existing eventproperties are done through the manager.
+* The eventpropertymanager is owned by the mappingwidget and the calls
+* to the remap-functions comes from MappingPopup.
+* @see EventProperty
+* @see MappingWidget
+* @see MappingPopup
+*/
 class EventPropertyManager {
 
 public:
 	EventPropertyManager();
 	~EventPropertyManager();
 
+	/** 
+	 * \brief Returns eventproperties
+	 *
+	 * Returns the eventproperties for the current active processor.
+	 * The active processor is the processor which is seletected in the combobox in the mappingwidget.
+	 * 
+	 * @return std::vector<EventProperty*> A vector with the eventproperties for the active processor.
+	 */
 	std::vector<EventProperty*> getEventPropertiesFromMap();
 
+	/** 
+	 * \brief Updates the manager with eventproperties
+	 *
+	 * Sets the map of eventproperties to a new map of all current eventproperties.
+	 * This function is called every time a processor is added or removed from the workspace.
+	 * 
+	 * @param std::map<std::string, std::vector<EventProperty * > > eventPropertyMap The new map of eventpropeties
+	 * @return void
+	 */
 	void setEventPropertyMap(std::map<std::string, std::vector<EventProperty*> > eventPropertyMap);
+	
+	/** 
+	 * \brief Sets the active processor
+	 *
+	 * Sets the active processor to the currently selected processor in the combobox in the mappingwidget.
+	 * The active processor is stored as a string of the processors identifier, which is also used as key in the eventPropertyMap.
+	 * 
+	 * @param std::string processorIdentifier The identifier of the selected processor
+	 * @return void
+	 */
 	void setActiveProcessor(std::string processorIdentifier){ activeProcessor_ = processorIdentifier; };
 
+	/** 
+	 * \brief Remaps an eventproperty with a mouseevent
+	 *
+	 * Changes the mapping of an eventproperty with a mouseevent and a modifier.
+	 * If another eventproperty is mapped with the same event and modifier, that eventpropertys mapping is unbound.
+	 * This function is called from the MappingPopup.
+	 * 
+	 * @param EventProperty * eventProperty The eventproperty to remap
+	 * @param MouseEvent::MouseButton button The buton for the new mouseevent
+	 * @param Event::Modifier modifier The modifier for the new mouseevent
+	 * @return void
+	 */
 	void changeMouseMapping(EventProperty* eventProperty, MouseEvent::MouseButton button, Event::Modifier modifier);
+	
+	/** 
+	 * \brief Remaps an eventproperty with a keyboardevent
+	 *
+	 * Changes the mapping of an eventproperty with a keyboardevent and a modifier.
+	 * If another eventproperty is mapped with the same event and modifier, that eventpropertys mapping is unbound.
+	 * This function is called from the MappingPopup.
+	 * 
+	 * @param EventProperty * eventProperty The eventproperty to remap
+	 * @param char button The button for the new keyboardevent
+	 * @param Event::Modifier modifier The modifier for the new keyboardevent
+	 * @return void
+	 */
 	void changeKeyMapping(EventProperty* eventProperty, char button, Event::Modifier modifier);
 
 private:
-	std::map<std::string, std::vector<EventProperty*> > eventPropertyMap_;
-	std::string activeProcessor_;
+	std::map<std::string, std::vector<EventProperty*> > eventPropertyMap_; ///< map<Processor identifier, vector of the processors eventproperties>
+	std::string activeProcessor_; ///< The identifier of the currently seletected processor in the combobox in the mappingwidget
 };
 
 } // namespace
