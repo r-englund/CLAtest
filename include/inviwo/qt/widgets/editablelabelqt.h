90755e5ffabe33d0999c460c35c8fc511c97fcf8
alejo849 <alejo849@student.liu.se>
2013-08-23 12:37:36 +0000

    Forgot the new files...

---
+++ include/inviwo/qt/widgets/editablelabelqt.h
@@ -0,0 +1,37 @@
+#ifndef EDITABLELABELQT_H
+#define EDITABLELABELQT_H
+
+#include <inviwo/qt/widgets/inviwoqtwidgetsdefine.h>
+#include <QLabel>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QMouseEvent>
+#include <QMenu>
+namespace inviwo {
+
+    class IVW_QTWIDGETS_API EditableLabelQt: public QWidget{
+    Q_OBJECT
+public:
+    EditableLabelQt(std::string text);
+    void edit();
+    std::string getText(){return text_;};
+
+public slots:
+    void editingOff();
+    void showContextMenu(const QPoint& pos);
+
+private:
+    QLabel* label_;
+    QLineEdit* lineEdit_;
+    std::string text_;
+    void generateWidget();
+    QMenu* contextMenu_;
+    void mouseDoubleClickEvent( QMouseEvent * event );
+    std::string shortenText();
+
+    signals:
+        void textChanged();
+};
+}//namespace
+
+#endif //EDITABLELABELQT_H
---
+++ src/qt/widgets/editablelabelqt.cpp
@@ -0,0 +1,74 @@
+#include <inviwo/qt/widgets/editablelabelqt.h>
+
+namespace inviwo{
+
+    EditableLabelQt::EditableLabelQt(std::string text){
+    text_ = text;
+    generateWidget();
+}
+
+void EditableLabelQt::generateWidget(){
+    QHBoxLayout* hLayout = new QHBoxLayout();
+    label_ = new QLabel();
+    label_->setText(QString::fromStdString(shortenText()));
+    lineEdit_ = new QLineEdit();
+    hLayout->addWidget(lineEdit_);
+    lineEdit_->hide();
+    lineEdit_->setAlignment(Qt::AlignLeft);
+    hLayout->addWidget(label_);
+    setLayout(hLayout);
+
+    contextMenu_ = new QMenu();
+    contextMenu_->addAction("Rename");
+
+    label_->setContextMenuPolicy(Qt::CustomContextMenu);
+  
+
+    connect(label_,SIGNAL(customContextMenuRequested(const QPoint&)),this,SLOT(showContextMenu(const QPoint&)));
+    connect(lineEdit_, SIGNAL(editingFinished()),this, SLOT(editingOff()));
+}
+
+void EditableLabelQt::edit(){
+    label_->hide();
+    lineEdit_->setText(QString::fromStdString(text_));
+    lineEdit_->show();
+    lineEdit_->setContentsMargins(0,0,0,0);
+    lineEdit_->setFocus();
+    lineEdit_->selectAll();
+}
+
+
+void EditableLabelQt::mouseDoubleClickEvent( QMouseEvent* e ){
+    edit();
+}
+
+void EditableLabelQt::editingOff(){
+    lineEdit_->hide();
+    text_ = (std::string)lineEdit_->text().toAscii();
+    label_->setText(QString::fromStdString(shortenText()));    
+    label_->show();
+    emit textChanged();
+}
+
+void EditableLabelQt::showContextMenu( const QPoint& pos ){
+    QPoint globalPos = label_->mapToGlobal(pos);
+    QAction* selecteditem = contextMenu_->exec(globalPos);
+    if (selecteditem == contextMenu_->actions().at(0)) {
+        edit();
+    }
+}
+
+std::string EditableLabelQt::shortenText(){
+    if (text_.length()>20) {
+        std::string shortText = text_.substr(0,10)+"..."+text_.substr(text_.length()-7,7);
+        label_->setToolTip(QString::fromStdString(text_));
+        return shortText;
+    }else{
+        label_->setToolTip(QString::fromStdString(""));
+        return text_;
+    }
+    
+}
+
+
+}//namespace
