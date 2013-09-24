8f06ec5a90e759caf5475f5d4b28988a908c751b
petersteneteg <peter@steneteg.se>
2013-09-24 14:49:42 +0000

    Added method to create a parallelepiped

--- include/inviwo/core/datastructures/geometry/basemeshcreator.h
+++ include/inviwo/core/datastructures/geometry/basemeshcreator.h
@@ -12,7 +12,11 @@ class IVW_CORE_API BaseMeshCreator {
 public:
     static BaseMeshRAM* rectangularPrism(vec3 posLlf, vec3 posUrb, vec3 texCoordLlf, 
                                         vec3 texCoordUrb, vec4 colorLlf, vec4 colorUrb);
-	static BaseMeshRAM* rectangle(vec3 posLl, vec3 posUr);
+	static BaseMeshRAM* parallelepiped(glm::vec3 pos, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3, 	
+									   glm::vec3 tex, glm::vec3 t1, glm::vec3 t2, glm::vec3 t3, 	
+									   glm::vec4 col, glm::vec4 c1, glm::vec4 c2, glm::vec4 c3); 	
+
+	static BaseMeshRAM* rectangle(glm::vec3 posLl, glm::vec3 posUr);
 };
 
 } // namespace
