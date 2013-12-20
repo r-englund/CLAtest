c9f8c730f9ac4324f870023b8586c8f8edacb38e
r-englund <rickard@r-englund.com>
2013-12-20 08:51:31 +0000

    Attatched ConsoleLogger to UnitTest to be able to see eventual logging message

--- apps/unittests/inviwounittest.cpp
+++ apps/unittests/inviwounittest.cpp
@@ -20,5 +20,7 @@ int main(int argc , char ** argv){
     InviwoApplication app(argc, argv, "unittest "+IVW_VERSION, IVW_DIR);
     app.initialize(&inviwo::registerAllModules);
 
+    inviwo::LogCentral::instance()->registerLogger(new inviwo::ConsoleLogger());
+	
     return inviwo::UnitTestsModule::runAllTests();
 }
