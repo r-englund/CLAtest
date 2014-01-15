db93987d2a6a067d3baa43a9cc5e7f30e22bb5f9
r-englund <rickard@r-englund.com>
2014-01-15 07:52:42 +0000

    Added a simple volume load test

--- include/inviwo/core/unittests/volume-test.h
+++ include/inviwo/core/unittests/volume-test.h
@@ -1,11 +1,33 @@
+#include <inviwo/core/datastructures/volume/volume.h>
+#include <inviwo/core/datastructures/volume/volumeram.h>
+#include <inviwo/core/resources/resourcemanager.h>
+#include <inviwo/core/io/datareaderfactory.h>
+#include <inviwo/core/resources/templateresource.h>
 
 #ifndef IVW_VOLUME_TEST_H
 #define IVW_VOLUME_TEST_H
 
 
-TEST(VolumeTest,test1){
+TEST(VolumeTest,SimpleLoadVolumeTest){
     EXPECT_TRUE(true);
-    //this is to be implemented
+    
+    std::string file = InviwoApplication::getPtr()->getPath(InviwoApplication::PATH_VOLUMES) + "/hydrogenatom.dat";
+
+    std::string fileExtension = URLParser::getFileExtension(file);
+    DataReaderType<Volume>* reader = DataReaderFactory::getRef().getReaderForTypeAndExtension<Volume>(fileExtension);
+    if(reader){
+        Volume* volume = reader->readMetaData(file);
+        ResourceManager::instance()->addResource(new TemplateResource<Volume>(file, volume));
+        const VolumeRAM* volumeRAM = volume->getRepresentation<VolumeRAM>();
+    }
+
+    ResourceManager::instance()->clearAllResources();
 }
+
+
+
+
+
+
 
 #endif
