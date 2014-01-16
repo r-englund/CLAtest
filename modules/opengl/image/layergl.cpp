dacd8884243a78ae5fca8270e5663d71b3d24cf2
eriksunden <eriksunden85@gmail.com>
2014-01-16 21:09:44 +0000

    Using GL_NEAREST when having 2D float textures.

--- modules/opengl/image/layergl.cpp
+++ modules/opengl/image/layergl.cpp
@@ -51,8 +51,17 @@ void LayerGL::initialize() {
         if(getLayerType() == DEPTH_LAYER){
             texture_ = new Texture2D(getDimension(), GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT24, glFormat.type, GL_NEAREST);
         }
-        else
-            texture_ = new Texture2D(getDimension(), glFormat, GL_NEAREST);
+        else{
+            //On Linux their might be an issue with float textures and linear filtering
+            GLenum filter = GL_LINEAR;
+            switch (glFormat.type) {
+                case GL_HALF_FLOAT:
+                case GL_FLOAT:
+                    filter = GL_NEAREST;
+                    break;
+            }
+            texture_ = new Texture2D(getDimension(), glFormat, filter);
+        }
     }
 }
 
