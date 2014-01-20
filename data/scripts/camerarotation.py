eb88d3c4ab56a12846ea1953bc8c0a21ff089c4c
r-englund <rickard@r-englund.com>
2014-01-20 14:07:46 +0000

    camerarotation.py now prints fps at end of run, capped at 60 fps?

--- data/scripts/camerarotation.py
+++ data/scripts/camerarotation.py
@@ -1,6 +1,9 @@
 # Inviwo Python script 
 import inviwo 
 import math 
+import time
+
+start = time.clock()
 
 d = 5
 steps = 120
@@ -17,3 +20,10 @@ for i in range(0, steps):
    z = 1.0*math.cos(r)
    inviwo.setCameraUp("EntryExitPoints","camera",(x,z,0))
 
+
+end = time.clock()
+fps = 2*steps / (end - start)
+
+fps = round(fps,2)
+
+print "fps: " + str(fps)
