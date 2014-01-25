83733f49cea584a6860e5bd5e74afd4c0282c856
r-englund <rickard@r-englund.com>
2014-01-25 16:43:21 +0000

    fixed loadworkspace example script

--- data/scripts/loadworkspace.py
+++ data/scripts/loadworkspace.py
@@ -1,5 +1,5 @@
 # Inviwo Python script 
 import inviwoqt 
 
-for i in range(0, 50):
-   inviwoqt.loadWorkspace("tests/simpleraycaster.inv")
+for i in range(0, 5):
+   inviwoqt.loadWorkspace( inviwo.getDataPath() + "/workspaces/tests/simpleraycaster.inv")
