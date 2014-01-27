b9512c2aaeae11f25aef084d200ffd1a311dc465
r-englund <rickard@r-englund.com>
2014-01-27 16:34:40 +0000

    removed unused code from inviwounittest.cpp

--- apps/unittests/inviwounittest.cpp
+++ apps/unittests/inviwounittest.cpp
@@ -3,12 +3,6 @@
     #pragma comment(linker, "/SUBSYSTEM:CONSOLE")
 #endif
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-#define DEBUG_NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
-#define new DEBUG_NEW
-
 #include <modules/unittests/unittestsmodule.h>
 
 #include <inviwo/core/common/inviwo.h>
@@ -20,10 +14,6 @@
 using namespace inviwo;
 
 int main(int argc , char ** argv){
-    _CrtSetReportMode( _CRT_WARN, _CRTDBG_MODE_FILE  | _CRTDBG_MODE_DEBUG);
-    _CrtSetReportFile( _CRT_WARN, _CRTDBG_FILE_STDOUT );
-    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
-    
     int ret = -1;
     inviwo::ConsoleLogger* logger = new inviwo::ConsoleLogger();
     { //scope for ivw app
