dadcd4b64f10019010f7824cbb02bceabd623267
eriksunden <eriksunden85@gmail.com>
2014-02-18 08:51:55 +0000

    Fixes for RegressionTest. Re-enabled ImageMixer.

--- modules/basegl/processors/imagemixer.cpp
+++ modules/basegl/processors/imagemixer.cpp
@@ -67,22 +67,23 @@ void ImageMixer::deinitialize() {
 void ImageMixer::process() {    
     ivwAssert(inport0_.getData()!=0, "Inport0 empty.");
     ivwAssert(inport1_.getData()!=0, "Inport1 empty.");
-    TextureUnit inportTexture0;
-    TextureUnit inportTexture1;
-    uvec2 csize = outport_.getData()->getDimension();    
-    bindColorTexture(inport0_, inportTexture0.getEnum());
-    bindColorTexture(inport1_, inportTexture1.getEnum());    
+    uvec2 csize = outport_.getData()->getDimension();
+
+    bindColorTexture(inport0_, GL_TEXTURE1);
+    TextureUnit::setZeroUnit();
+
+    bindColorTexture(inport1_, GL_TEXTURE2);
+    TextureUnit::setZeroUnit();
+
     activateAndClearTarget(outport_);
     shader_->activate();
-    shader_->setUniform("inport0_", inportTexture0.getUnitNumber());
-    shader_->setUniform("inport1_", inportTexture1.getUnitNumber());
+    shader_->setUniform("inport0_", 1);
+    shader_->setUniform("inport1_", 2);
     shader_->setUniform("alpha_", alpha_.get());
     shader_->setUniform("screenDimRCP_", vec2(1.f / csize[0], 1.f / csize[1]) );
     renderImagePlaneRect();
     shader_->deactivate();
     deactivateCurrentTarget();
-    unbindColorTexture(inport0_);
-    unbindColorTexture(inport1_);
 }
 
 } // namespace
