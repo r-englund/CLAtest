81066bd3952e1b027a6dfd5440c39377df3b5043
danjobanjo <daniel@copax.se>
2014-02-26 14:40:46 +0000

    Core: Added BufferInport and BufferOutport

---
+++ include/inviwo/core/ports/bufferport.h
@@ -0,0 +1,76 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ * Version 0.6b
+ *
+ * Copyright (c) 2012-2014 Inviwo Foundation
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Main file authors: Daniel Jönsson
+ *
+ *********************************************************************************/
+
+#ifndef IVW_BUFFER_PORT_H
+#define IVW_BUFFER_PORT_H
+
+#include <inviwo/core/common/inviwocoredefine.h>
+#include <inviwo/core/ports/datainport.h>
+#include <inviwo/core/ports/dataoutport.h>
+#include <inviwo/core/datastructures/buffer/buffer.h>
+
+
+namespace inviwo {
+
+class IVW_CORE_API BufferInport : public DataInport<Buffer> {
+
+public:
+    BufferInport(std::string identifier,
+        PropertyOwner::InvalidationLevel invalidationLevel=PropertyOwner::INVALID_OUTPUT);
+    virtual ~BufferInport();
+
+    void initialize();
+    void deinitialize();
+
+    uvec3 getColorCode() const;
+    static uvec3 colorCode;
+    virtual std::string getClassName() const {return "BufferInport";}
+};
+
+class IVW_CORE_API BufferOutport : public DataOutport<Buffer> {
+
+public:
+    BufferOutport(std::string identifier, PropertyOwner::InvalidationLevel invalidationLevel=PropertyOwner::INVALID_OUTPUT);
+    BufferOutport(std::string identifier, size_t size, const DataFormatBase* format = DataFLOAT32::get(),
+        PropertyOwner::InvalidationLevel invalidationLevel=PropertyOwner::INVALID_OUTPUT);
+    virtual ~BufferOutport();
+
+    void initialize();
+    void deinitialize();
+
+    uvec3 getColorCode() const;
+    virtual std::string getClassName() const {return "BufferOutport";}
+};
+
+} // namespace
+
+#endif // IVW_BUFFER_PORT_H
