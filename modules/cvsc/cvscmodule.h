067ca47b0c99eab892bda1cead0ce4a154dfe0c2
eriksunden <eriksunden85@gmail.com>
2014-03-03 14:55:54 +0000

    CVSC: Checked In CVSC processor code.

---
+++ modules/cvsc/cvscmodule.h
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * Copyright (C) 2014 ContextVision AB
+ * All Rights Reserved.
+ * 
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * No part of this software may be reproduced or transmitted in any
+ * form or by any means including photocopying or recording without
+ * written permission of the copyright owner.
+ *
+ * Primary author : Erik Sundén
+ *
+ **********************************************************************/
+
+#ifndef IVW_CVSCMODULE_H
+#define IVW_CVSCMODULE_H
+
+#include <modules/cvsc/cvscmoduledefine.h>
+#include <inviwo/core/common/inviwomodule.h>
+
+namespace inviwo {
+
+class IVW_MODULE_CVSC_API CVSCModule : public InviwoModule {
+
+public:
+    CVSCModule();
+
+};
+
+} // namespace
+
+#endif // IVW_CVSCMODULE_H
