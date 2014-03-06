37153a0af688be47794a7baddc3f12025c4fd8f4
r-englund <rickard@r-englund.com>
2014-03-06 08:24:55 +0000

    Python script updates

--- data/scripts/camerarotation.py
+++ data/scripts/camerarotation.py
@@ -5,20 +5,21 @@ import time
 
 start = time.clock()
 
+scale = 1;
 d = 5
 steps = 120
 for i in range(0, steps):
    r = (2 * 3.14 * i) / steps
    x = d*math.sin(r)
    z = -d*math.cos(r)
-   inviwo.setPropertyValue("EntryExitPoints","camera",((x,3,z),(0,0,0),(0,1,0)))
+   inviwo.setPropertyValue("EntryExitPoints","camera",((x*scale,3*scale,z*scale),(0,0,0),(0,1,0)))
 
 
 for i in range(0, steps):
    r = (2 * 3.14 * i) / (steps)
    x = 1.0*math.sin(r)
    z = 1.0*math.cos(r)
-   inviwo.setCameraUp("EntryExitPoints","camera",(x,z,0))
+   inviwo.setCameraUp("EntryExitPoints","camera",(x*scale,z*scale,0))
 
 
 end = time.clock()
