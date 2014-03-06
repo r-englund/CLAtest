8e370d84f2dec14bf832d5d83e54acdc7c229389
r-englund <rickard@r-englund.com>
2014-03-06 08:24:55 +0000

    Python script updates

--- data/scripts/canvassnapshot.py
+++ data/scripts/canvassnapshot.py
@@ -1,19 +1,4 @@
 # Inviwo Python script 
 import inviwo 
 
-inviwo.snapshot("test.bmp") 
-inviwo.snapshot("test.jpg") 
-inviwo.snapshot("test.j2k") 
-inviwo.snapshot("test.ico") 
-inviwo.snapshot("test.hdr") 
-inviwo.snapshot("test.gif") 
-inviwo.snapshot("test.exr") 
-inviwo.snapshot("test.jng") 
-inviwo.snapshot("test.pbm") 
-inviwo.snapshot("test.pgm") 
-inviwo.snapshot("test.ppm") 
-inviwo.snapshot("test.tiff") 
-inviwo.snapshot("test.wbmp") 
-inviwo.snapshot("test.xpm") 
-inviwo.snapshot("test.png")  
-inviwo.snapshot("test.pfm") 
+inviwo.snapshotCanvas(0,"snapshot.png") # this will take a snapshot of the first canvas, or fail with an error if there is no canvas in the current workspace 
