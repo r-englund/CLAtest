979e9b05f125d8df32d28060a9e2d7469e4e93f2
petersteneteg <peter@steneteg.se>
2014-03-19 08:50:03 +0000

    revert entryexitpoints.cpp

--- modules/basegl/processors/entryexitpoints.cpp
+++ modules/basegl/processors/entryexitpoints.cpp
@@ -123,7 +123,7 @@ void EntryExitPoints::process() {
         if (true || tmpEntryPoints_ == NULL ||
             tmpEntryPoints_->getDimension() != entryPort_.getDimension() ||
             tmpEntryPoints_->getImageType() != entryPort_.getData()->getImageType() ||
-            tmpEntryPoints_->getDataFormat() != entryPort_.getData()->getDataFormat() || true ) {
+            tmpEntryPoints_->getDataFormat() != entryPort_.getData()->getDataFormat()) {
             delete tmpEntryPoints_;
             tmpEntryPoints_ = new Image(entryPort_.getDimension(), entryPort_.getData()->getImageType(), entryPort_.getData()->getDataFormat());
         }
