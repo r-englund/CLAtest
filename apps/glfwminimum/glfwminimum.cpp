fdfbd47209603b55c0e4f619f740cc573ab48623
eriksunden <eriksunden85@gmail.com>
2014-04-04 06:16:29 +0000

    glfwminimum path fix.

--- apps/glfwminimum/glfwminimum.cpp
+++ apps/glfwminimum/glfwminimum.cpp
@@ -117,7 +117,7 @@ int main(int argc, char** argv) {
     if (cmdparser->getLoadWorkspaceFromArg())
         workspace = cmdparser->getWorkspacePath();
     else
-        workspace = inviwoApp.getPath(InviwoApplication::PATH_WORKSPACES, "tests/simpeslicergl.inv");
+        workspace = inviwoApp.getPath(InviwoApplication::PATH_WORKSPACES, "tests/simpleslicergl.inv");
 
     IvwDeserializer xmlDeserializer(workspace);
     processorNetwork->deserialize(xmlDeserializer);
