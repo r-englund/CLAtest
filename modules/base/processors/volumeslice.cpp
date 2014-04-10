0584f2bc38f5c1095401506c29708d89a360fa93
eriksunden <eriksunden85@gmail.com>
2014-04-10 23:57:37 +0000

    VolumeSlicer(CPU/GL) updates and replace VolumePortInspector with CPU slicer to avoid OpenGL upload.

--- modules/base/processors/volumeslice.cpp
+++ modules/base/processors/volumeslice.cpp
@@ -43,7 +43,7 @@ VolumeSlice::VolumeSlice()
       inport_("volume.inport"),
       outport_("image.outport", COLOR_ONLY),
       coordinatePlane_("coordinatePlane", "Coordinate Plane"),
-      sliceNumber_("sliceNumber", "Slice Number", 1, 1, 256)
+      sliceNumber_("sliceNumber", "Slice Number", 4, 1, 8)
 {
     addPort(inport_);
     addPort(outport_);
@@ -53,9 +53,17 @@ VolumeSlice::VolumeSlice()
     coordinatePlane_.setSelectedIndex(0);
     addProperty(coordinatePlane_);
     addProperty(sliceNumber_);
+    addInteractionHandler(new VolumeSliceInteractationHandler(this));
 }
 
-VolumeSlice::~VolumeSlice() {}
+VolumeSlice::~VolumeSlice() {
+    const std::vector<InteractionHandler*>& interactionHandlers = getInteractionHandlers();
+    for(size_t i=0; i<interactionHandlers.size(); ++i) {
+        InteractionHandler* handler = interactionHandlers[i];
+        removeInteractionHandler(handler);
+        delete handler;
+    }
+}
 
 void VolumeSlice::initialize() {
     Processor::initialize();
@@ -65,21 +73,36 @@ void VolumeSlice::deinitialize() {
     Processor::deinitialize();
 }
 
+void VolumeSlice::shiftSlice(int shift){
+    int newSlice = sliceNumber_.get()+shift;
+    if(newSlice >= sliceNumber_.getMinValue() && newSlice <= sliceNumber_.getMaxValue())
+        sliceNumber_.set(newSlice);
+}
+
 void VolumeSlice::process() {
     uvec3 dims = inport_.getData()->getDimension();
 
     switch (coordinatePlane_.get())
     {
         case XY:
-            sliceNumber_.setMaxValue(static_cast<int>(dims.z));
+            if(dims.z!=sliceNumber_.getMaxValue()){
+                sliceNumber_.setMaxValue(static_cast<int>(dims.z));
+                sliceNumber_.set(static_cast<int>(dims.z)/2);
+            }
             break;
 
         case XZ:
-            sliceNumber_.setMaxValue(static_cast<int>(dims.y));
+            if(dims.y!=sliceNumber_.getMaxValue()){
+                sliceNumber_.setMaxValue(static_cast<int>(dims.y));
+                sliceNumber_.set(static_cast<int>(dims.y)/2);
+            }
             break;
 
         case YZ:
-            sliceNumber_.setMaxValue(static_cast<int>(dims.x));
+            if(dims.x!=sliceNumber_.getMaxValue()){
+                sliceNumber_.setMaxValue(static_cast<int>(dims.x));
+                sliceNumber_.set(static_cast<int>(dims.x)/2);
+            }
             break;
     }
 
@@ -90,6 +113,47 @@ void VolumeSlice::process() {
     outImage->getColorLayer()->addRepresentation(sliceImage);
 
     outport_.setData(outImage);
+}
+
+VolumeSlice::VolumeSliceInteractationHandler::VolumeSliceInteractationHandler(VolumeSlice* vs) 
+: InteractionHandler()
+, wheelEvent_(MouseEvent::MOUSE_BUTTON_NONE, InteractionEvent::MODIFIER_NONE)
+, upEvent_('W',InteractionEvent::MODIFIER_NONE, KeyboardEvent::KEY_STATE_PRESS)
+, downEvent_('S',InteractionEvent::MODIFIER_NONE, KeyboardEvent::KEY_STATE_PRESS)
+, slicer_(vs) {
+}
+
+void VolumeSlice::VolumeSliceInteractationHandler::invokeEvent(Event* event){
+    KeyboardEvent* keyEvent = dynamic_cast<KeyboardEvent*>(event);
+    if (keyEvent) {
+        int button = keyEvent->button();
+        KeyboardEvent::KeyState state = keyEvent->state();
+        InteractionEvent::Modifier modifier = keyEvent->modifier();
+
+        if (button == upEvent_.button()
+            && modifier == upEvent_.modifier()
+            && state == KeyboardEvent::KEY_STATE_PRESS)
+            slicer_->shiftSlice(1);
+        else if (button == downEvent_.button()
+            && modifier == downEvent_.modifier()
+            && state == KeyboardEvent::KEY_STATE_PRESS)
+            slicer_->shiftSlice(-1);
+
+        return;
+    }
+
+    MouseEvent* mouseEvent = dynamic_cast<MouseEvent*>(event);
+    if (mouseEvent) {
+        MouseEvent::MouseState state = mouseEvent->state();
+        InteractionEvent::Modifier modifier = mouseEvent->modifier();
+
+        if (modifier == wheelEvent_.modifier()
+            && state == MouseEvent::MOUSE_STATE_WHEEL) {
+                int steps = mouseEvent->wheelSteps();
+                slicer_->shiftSlice(steps);
+        }
+        return;
+    }
 }
 
 } // inviwo namespace
