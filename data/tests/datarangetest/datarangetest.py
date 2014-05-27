97a504107a92b5d7964a5f2006756f28177788d5
petersteneteg <peter@steneteg.se>
2014-05-27 13:49:16 +0000

    Gathered all the data range test data to a common folder under data/tests

---
+++ data/tests/datarangetest/datarangetest.py
@@ -0,0 +1,12 @@
+# Inviwo Python script 
+import inviwo 
+import inviwoqt 
+import os
+
+path = inviwo.getDataPath()+os.sep+"tests"+os.sep+"datarangetest"
+
+for f in os.listdir(path+os.sep+"datasets"):
+    if ".dat" in f:
+        print(f)
+        inviwo.setPropertyValue("VolumeSource", "filename",path+os.sep+"datasets" + os.sep + f)
+        inviwo.snapshot(path+os.sep+"testdata"+os.sep+f+".jpg")
