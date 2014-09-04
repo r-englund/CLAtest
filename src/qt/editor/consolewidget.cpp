fbe4bdef05d8fb6a0bba5656b17de6d3394420d8
r-englund <rickard@r-englund.com>
2014-09-04 12:01:09 +0000

    Added log message count to console

--- src/qt/editor/consolewidget.cpp
+++ src/qt/editor/consolewidget.cpp
@@ -33,21 +33,69 @@
 #include <QMenu>
 #include <QTextCursor>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTextEdit>
+#include <QKeyEvent>
+#include <QLabel>
 
 #include <inviwo/qt/editor/consolewidget.h>
 
 namespace inviwo {
 
 ConsoleWidget::ConsoleWidget(QWidget* parent) : InviwoDockWidget(tr("Console"), parent)
-    , infoTextColor_(153,153,153), warnTextColor_(221,165,8), errorTextColor_(255,107,107) {
+    , infoTextColor_(153,153,153), warnTextColor_(221,165,8), errorTextColor_(255,107,107)
+    , errorsLabel_(NULL), warningsLabel_(NULL), infoLabel_(NULL)
+    , numErrors_(0), numWarnings_(0), numInfos_(0)
+{
     setObjectName("ConsoleWidget");
     setAllowedAreas(Qt::BottomDockWidgetArea);
     textField_ = new QTextEdit(this);
     textField_->setReadOnly(true);
     textField_->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-    setWidget(textField_);
     textField_->setContextMenuPolicy(Qt::CustomContextMenu);
     connect(textField_, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(showContextMenu(const QPoint&)));
+
+    QHBoxLayout *statusBar = new QHBoxLayout();
+
+    errorsLabel_ = new QLabel("0", this);
+    warningsLabel_ = new QLabel("0", this);
+    infoLabel_ = new QLabel("0", this);
+
+    QFrame* line1 = new QFrame();
+    QFrame* line2 = new QFrame();
+    QFrame* line3 = new QFrame();
+    line1->setFrameShape(QFrame::VLine);
+    line2->setFrameShape(QFrame::VLine);
+    line3->setFrameShape(QFrame::VLine);
+    
+    line1->setFrameShadow(QFrame::Raised);
+    line2->setFrameShadow(QFrame::Raised);
+    line3->setFrameShadow(QFrame::Raised);
+
+    statusBar->addWidget(new QLabel( "<img width='16' height='16' src=':/icons/error.png'>", this));
+    statusBar->addWidget(errorsLabel_);
+    statusBar->addWidget(line1);
+    statusBar->addWidget(new QLabel( "<img width='16' height='16' src=':/icons/warning.png'>", this));
+    statusBar->addWidget(warningsLabel_);
+    statusBar->addWidget(line2);
+    statusBar->addWidget(new QLabel( "<img width='16' height='16' src=':/icons/about.png'>", this));
+    statusBar->addWidget(infoLabel_);
+    statusBar->addWidget(line3);
+    
+
+    statusBar->addItem(new QSpacerItem(40, 1, QSizePolicy::Expanding, QSizePolicy::Minimum));
+    
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(textField_);
+    layout->addLayout(statusBar);
+
+    layout->setContentsMargins(3, 0, 0, 3);
+
+
+    QWidget* w = new QWidget();
+    w->setLayout(layout);
+    setWidget(w);
+
 }
 
 ConsoleWidget::~ConsoleWidget() {
@@ -60,7 +108,13 @@ void ConsoleWidget::showContextMenu(const QPoint& pos) {
     menu->addAction(clearAction);
     QAction* result = menu->exec(QCursor::pos());
 
-    if (result == clearAction) textField_->clear();
+    if (result == clearAction) {
+        textField_->clear();
+        errorsLabel_->setText("0");
+        warningsLabel_->setText("0");
+        infoLabel_->setText("0");
+        numErrors_ = numWarnings_ = numInfos_ = 0;
+    }
 
     delete menu;
 }
@@ -74,11 +128,13 @@ void ConsoleWidget::log(std::string logSource, unsigned int logLevel, const char
         case Info:
             textField_->setTextColor(infoTextColor_);
             message = QString::fromStdString("(" + logSource + ") " + logMsg);
+            infoLabel_->setText(toString(++numInfos_).c_str());
             break;
 
         case Warn:
             textField_->setTextColor(warnTextColor_);
             message = QString::fromStdString("(" + logSource + ") " + logMsg);
+            warningsLabel_->setText(toString(++numWarnings_).c_str());
             break;
 
         case Error: {
@@ -86,7 +142,8 @@ void ConsoleWidget::log(std::string logSource, unsigned int logLevel, const char
             std::ostringstream lineNumberStr;
             lineNumberStr << lineNumber;
             message = QString::fromStdString("(" + logSource + ") [" + std::string(fileName) +
-                                             ", " + lineNumberStr.str() + "]: " + logMsg);
+                ", " + lineNumberStr.str() + "]: " + logMsg);
+            errorsLabel_->setText(toString(++numErrors_).c_str());
             break;
         }
 
@@ -102,8 +159,13 @@ void ConsoleWidget::log(std::string logSource, unsigned int logLevel, const char
 }
 
 void ConsoleWidget::keyPressEvent(QKeyEvent* e) {
-    if (e->key() == Qt::Key_E && e->modifiers() == Qt::ControlModifier)
+    if (e->key() == Qt::Key_E && e->modifiers() == Qt::ControlModifier){
         textField_->clear();
+        errorsLabel_->setText("0");
+        warningsLabel_->setText("0");
+        infoLabel_->setText("0");
+        numErrors_ = numWarnings_ = numInfos_ = 0;
+    }
 }
 
 } // namespace
