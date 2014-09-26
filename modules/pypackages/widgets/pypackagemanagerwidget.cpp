305291c147c1115cd1490343ec537e8d8c786980
danjobanjo <daniel@copax.se>
2014-09-26 08:24:29 +0000

    pypackages: Fixed minor compile error

--- modules/pypackages/widgets/pypackagemanagerwidget.cpp
+++ modules/pypackages/widgets/pypackagemanagerwidget.cpp
@@ -37,7 +37,7 @@
 #include <QHeaderView>
 #include <QSignalMapper>
 
-#ifdef USING_QT5 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
 #define TABLE_WIDGET_SET_RESIZE_MODE setSectionResizeMode
 #else
 #define TABLE_WIDGET_SET_RESIZE_MODE setResizeMode
