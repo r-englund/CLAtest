a59ef48aee5d63c782b02d6b6cda863772076abd
eriksunden <eriksunden85@gmail.com>
2014-10-10 14:09:24 +0000

    Half precision fixes.

--- include/inviwo/core/util/formats.h
+++ include/inviwo/core/util/formats.h
@@ -545,7 +545,11 @@ public:
 /*---------------Single Value Formats------------------*/
 
 // Floats
-typedef GenericDataFormat(half_float::half)     DataFLOAT16;
+#ifdef __unix
+typedef DataFormat<glm::f32, 16>  DataFLOAT16;
+#else
+typedef GenericDataFormat(half_float::half) DataFLOAT16;
+#endif
 typedef GenericDataFormat(glm::f32) DataFLOAT32;
 typedef GenericDataFormat(glm::f64) DataFLOAT64;
 
@@ -566,8 +570,12 @@ typedef GenericDataFormat(glm::u64)  DataUINT64;
 /*---------------Vec2 Formats--------------------*/
 
 // Floats
+#ifdef __unix
+typedef DataFormat<glm::f32vec2, 32>  DataVec2FLOAT16;
+#else
 typedef glm::detail::tvec2<half_float::half, glm::defaultp> f16vec2;
 typedef GenericDataFormat(f16vec2) DataVec2FLOAT16;
+#endif
 typedef GenericDataFormat(glm::f32vec2) DataVec2FLOAT32;
 typedef GenericDataFormat(glm::f64vec2) DataVec2FLOAT64;
 
@@ -588,8 +596,12 @@ typedef GenericDataFormat(glm::u64vec2) DataVec2UINT64;
 /*---------------Vec3 Formats--------------------*/
 
 // Floats
+#ifdef __unix
+typedef DataFormat<glm::f32vec3, 48>  DataVec3FLOAT16;
+#else
 typedef glm::detail::tvec3<half_float::half, glm::defaultp> f16vec3;
 typedef GenericDataFormat(f16vec3) DataVec3FLOAT16;
+#endif
 typedef GenericDataFormat(glm::f32vec3) DataVec3FLOAT32;
 typedef GenericDataFormat(glm::f64vec3) DataVec3FLOAT64;
 
@@ -610,8 +622,12 @@ typedef GenericDataFormat(glm::u64vec3) DataVec3UINT64;
 /*---------------Vec4 Value Formats------------------*/
 
 // Floats
+#ifdef __unix
+typedef DataFormat<glm::f32vec4, 64>  DataVec4FLOAT16;
+#else
 typedef glm::detail::tvec4<half_float::half, glm::defaultp> f16vec4;
 typedef GenericDataFormat(f16vec4) DataVec4FLOAT16;
+#endif
 typedef GenericDataFormat(glm::f32vec4) DataVec4FLOAT32;
 typedef GenericDataFormat(glm::f64vec4) DataVec4FLOAT64;
 
