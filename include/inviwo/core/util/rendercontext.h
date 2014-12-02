0d060fd47382f7749e5ff3641499144af89d3375
Peter Steneteg <peter@steneteg.se>
2014-12-02 16:42:13 +0100

    Opengl: new Singleton render context

---
+++ include/inviwo/core/util/rendercontext.h
@@ -0,0 +1,65 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ * Version 0.6b
+ *
+ * Copyright (c) 2012-2014 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Contact: <Author>
+ *
+ *********************************************************************************/
+
+#ifndef IVW_RENDERCONTEXT_H
+#define IVW_RENDERCONTEXT_H
+
+#include <inviwo/core/common/inviwocoredefine.h>
+#include <inviwo/core/common/inviwo.h>
+#include <inviwo/core/util/singleton.h>
+
+namespace inviwo {
+
+class Canvas;
+/**
+ * \class RenderContext
+ *
+ * \brief VERY_BRIEFLY_DESCRIBE_THE_CLASS
+ *
+ * DESCRIBE_THE_CLASS
+ */
+class IVW_CORE_API RenderContext : public Singleton<RenderContext>{ 
+public:
+    RenderContext();
+    virtual ~RenderContext();
+
+    void setDefaultRenderContext(Canvas* canvas);
+    void activateDefaultRenderContext() const;
+
+private: 
+    Canvas* defaultContext_;
+};
+
+} // namespace
+
+#endif // IVW_RENDERCONTEXT_H
+
