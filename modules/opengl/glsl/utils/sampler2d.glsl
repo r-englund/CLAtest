1baf7192497e1fb861b5a54fe6cb5f29b24942bd
Daniel Jönsson <daniel@copax.se>
2014-12-03 16:29:47 +0100

    OpenGL: Added missing content in sample2d.glsl

--- modules/opengl/glsl/utils/sampler2d.glsl
+++ modules/opengl/glsl/utils/sampler2d.glsl
@@ -30,3 +30,15 @@
  *
  *********************************************************************************/
 
+uniform vec2 screenDim_;
+uniform vec2 screenDimRCP_;
+
+#include "utils/structs.glsl"
+
+vec4 textureLookup2Dnormalized(TEXTURE_TYPE tex, TEXTURE_PARAMETERS textureParams, vec2 samplePos) {
+    return texture(tex, samplePos);
+}
+
+vec4 textureLookup2Dscreen(TEXTURE_TYPE tex, TEXTURE_PARAMETERS textureParams, vec2 samplePos) {
+    return texture(tex, samplePos*screenDimRCP_);
+}
