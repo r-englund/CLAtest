6c6a23c1e98b9e696da6ef518eae1facc272bd44
eriksunden <eriksunden85@gmail.com>
2014-12-03 12:57:00 +0100

    CMake Qt elated linking corrections. Minor warning removal.

--- modules/openglqt/depends.cmake
+++ modules/openglqt/depends.cmake
@@ -6,7 +6,7 @@ set(dependencies
 )
 
 if(DESIRED_QT_VERSION MATCHES 5)
-    list(APPEND dependencies Qt5OpenGL)
+    list(APPEND dependencies Qt5OpenGL Qt5Help)
 else()
     list(APPEND dependencies Qt)
 endif()
--- modules/python3qt/depends.cmake
+++ modules/python3qt/depends.cmake
@@ -6,3 +6,9 @@ set(dependencies
     InviwoQtWidgets
     InviwoQtEditor
 )
+
+if(DESIRED_QT_VERSION MATCHES 5)
+    list(APPEND dependencies Qt5Help)
+else()
+    list(APPEND dependencies Qt)
+endif()
