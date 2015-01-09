50cdcbab9106cf1092a1a197abaaabe8b64e2a88
eriksunden <eriksunden85@gmail.com>
2015-01-09 09:53:46 +0100

    Started on a VolumeCombine processor for multimodal management.

---
+++ modules/base/processors/volumecombine.h
@@ -0,0 +1,73 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ * Version 0.6b
+ *
+ * Copyright (c) 2013-2014 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Contact: Erik Sundén
+ *
+ *********************************************************************************/
+
+#ifndef IVW_VOLUMECOMBINE_H
+#define IVW_VOLUMECOMBINE_H
+
+#include <modules/base/basemoduledefine.h>
+#include <inviwo/core/common/inviwo.h>
+#include <inviwo/core/ports/volumeport.h>
+#include <inviwo/core/processors/processor.h>
+#include <inviwo/core/properties/boolproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+
+namespace inviwo {
+
+class IVW_MODULE_BASE_API VolumeCombine : public Processor {
+public:
+    VolumeCombine();
+    ~VolumeCombine();
+
+    InviwoProcessorInfo();
+
+    void initialize();
+    void deinitialize();
+
+protected:
+    virtual void process();
+
+private:
+    VolumeInport inport1_;
+    VolumeInport inport2_;
+    VolumeInport inport3_;
+    VolumeInport inport4_;
+    VolumeOutport outport_;
+
+    BoolProperty enabled_;
+    IntProperty passThroughSelector_;
+
+    std::vector<VolumeInport*> inports_;
+};
+
+}
+
+#endif //IVW_VOLUMECOMBINE_H
