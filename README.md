ca26270cdec566286851eb8564add197e6cdb9e8
eriksunden <eriksunden85@gmail.com>
2015-01-14 08:56:23 +0100

    Updated general information

---
+++ README.md
@@ -0,0 +1,11 @@
+# Inviwo - Interactive Visualization Workshop
+
+Inviwo is a software framework for rapid visualization prototyping.
+Below follow some general information about the framework:
+
+ - `Freely available under the Simplified BSD License.
+ - `Cross-platform and runs on Windows, Linux and Mac OS X.
+ - `Easily extendable through inclusion of external modules and projects.
+ - `The core is written in only C/C++, with minor dependencies.
+ - `Modern graphics hardware computation is supported through provided OpenGL (core) and OpenCL modules.
+ - `Python scripting and computation is supported through provided modules.
--- apps/inviwo/inviwomainwindow.cpp
+++ apps/inviwo/inviwomainwindow.cpp
@@ -602,10 +602,8 @@ void InviwoMainWindow::showAboutBox() {
                      visualizations.<br>It is licensed under the Simplified BSD license.</p>");
     aboutText.append("<p><b>Core Team:</b><br>");
     aboutText.append(
-        "Erik Sund&eacute;n, Daniel J&ouml;nsson, Peter Steneteg, Rickard Englund,<br>"
+        "Erik Sund&eacute;n, Daniel J&ouml;nsson, Martin Falk, Peter Steneteg, Rickard Englund,<br>"
         "Sathish Kottravel, Timo Ropinski</p>");
-    aboutText.append("<p><b>Additional Developers:</b><br>");
-    aboutText.append("Martin Falk</p>");
     aboutText.append("<p><b>Former Developers:</b><br>");
     aboutText.append("Alexander Johansson, Andreas Valter, Johan Nor&eacute;n, Emanuel Winblad, "
         "Hans-Christian Helltegen, Viktor Axelsson</p>");
