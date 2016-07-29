d73e31ad2905a56a138e40ac7441fffce7b23c34
Peter Steneteg <peter@steneteg.se>
2016-07-29 08:27:58 +0200

    Merge fix

---
+++ src/core/interaction/events/gestureevent.cpp
@@ -0,0 +1,67 @@
+/*********************************************************************************
+ *
+ * Inviwo - Interactive Visualization Workshop
+ *
+ * Copyright (c) 2014-2016 Inviwo Foundation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
+#include <inviwo/core/interaction/events/gestureevent.h>
+
+namespace inviwo {
+
+GestureEvent::GestureEvent(vec2 deltaPos, double deltaDistance, GestureType type,
+                           GestureState state, int numFingers, vec2 screenPosNorm, uvec2 canvasSize)
+    : InteractionEvent()
+    , type_(type)
+    , state_(state)
+    , numFingers_(numFingers)
+    , deltaPos_(deltaPos)
+    , deltaDistance_(deltaDistance)
+    , screenPosNorm_(screenPosNorm)
+    , canvasSize_(canvasSize) {}
+
+GestureEvent* GestureEvent::clone() const { return new GestureEvent(*this); }
+
+inviwo::vec2 GestureEvent::deltaPos() const { return deltaPos_; }
+
+double GestureEvent::deltaDistance() const { return deltaDistance_; }
+
+inviwo::GestureType GestureEvent::type() const { return type_; }
+
+inviwo::GestureState GestureEvent::state() const { return state_; }
+
+int GestureEvent::numFingers() const { return numFingers_; }
+
+inviwo::vec2 GestureEvent::screenPosNormalized() const { return screenPosNorm_; }
+
+inviwo::vec2 GestureEvent::canvasSize() const { return canvasSize_; }
+
+void GestureEvent::modify(vec2 posNorm) { screenPosNorm_ = posNorm; }
+
+uint64_t GestureEvent::hash() const {
+    return chash();
+}
+
+}  // namespace
